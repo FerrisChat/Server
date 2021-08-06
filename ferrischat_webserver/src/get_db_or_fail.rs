1e8f112da51c201dcce025bfd4b4e220e63ecebd
0/0 <zero@imaskeleton.me>
2021-08-05 20:32:12 -0600

    jsonify the error response for missing DB

--- ferrischat_webserver/src/get_db_or_fail.rs
+++ ferrischat_webserver/src/get_db_or_fail.rs
@@ -1,10 +1,15 @@
 #[macro_export]
 macro_rules! get_db_or_fail {
     () => {{
+        use ferrischat_common::types::InternalServerErrorJson;
         use ferrischat_db::DATABASE_POOL;
         match DATABASE_POOL.get() {
             Some(db) => db,
-            None => return HttpResponse::InternalServerError().body("Database pool was not found"),
+            None => {
+                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
+                    reason: "Database pool was not found".to_string(),
+                })
+            }
         }
     }};
 }
