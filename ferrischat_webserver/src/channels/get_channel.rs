9a5adec665a33ecf7edb64771ed1f92bfe339027
hydrostaticcog <hydro@hydrostaticcog.me>
2021-08-05 22:37:02 -0500

    add GET to channel and user endpoints

--- ferrischat_webserver/src/channels/get_channel.rs
+++ ferrischat_webserver/src/channels/get_channel.rs
@@ -1,6 +1,30 @@
-use actix_web::{web::Path, HttpResponse, Responder};
+use actix_web::{
+    web::{Json, Path},
+    HttpResponse, Responder,
+};
+use ferrischat_common::types::{Channel, InternalServerErrorJson};
+use ferrischat_macros::{bigdecimal_to_u128, get_db_or_fail};
+use num_traits::cast::ToPrimitive;
+use sqlx::types::BigDecimal;
 
 /// GET /api/v1/channels/{id}
 pub async fn get_channel(Path(channel_id): Path<i64>) -> impl Responder {
-    HttpResponse::Found().body("found channel test")
+    let db = get_db_or_fail!();
+    let channel_id = BigDecimal::from(channel_id);
+    let resp = sqlx::query!("SELECT * FROM channels WHERE id = $1", channel_id)
+        .fetch_optional(db)
+        .await;
+
+    match resp {
+        Ok(resp) => match resp {
+            Some(channel) => HttpResponse::Ok().json(Channel {
+                id: bigdecimal_to_u128!(channel.id),
+                name: channel.name,
+            }),
+            None => HttpResponse::NotFound().finish(),
+        },
+        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
+            reason: format!("database returned a error: {}", e),
+        }),
+    }
 }
--- ferrischat_webserver/src/users/get_user.rs
+++ ferrischat_webserver/src/users/get_user.rs
@@ -1,6 +1,32 @@
-use actix_web::{web::Path, HttpResponse, Responder};
+use actix_web::{
+    web::{Json, Path},
+    HttpResponse, Responder,
+};
+use ferrischat_common::types::{User, InternalServerErrorJson};
+use ferrischat_macros::{bigdecimal_to_u128, get_db_or_fail};
+use num_traits::cast::ToPrimitive;
+use sqlx::types::BigDecimal;
 
 /// GET /api/v1/users/{id}
 pub async fn get_user(Path(user_id): Path<i64>) -> impl Responder {
-    HttpResponse::Found().body("found user test")
+    let db = get_db_or_fail!();
+    let user_id = BigDecimal::from(user_id);
+    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", user_id)
+        .fetch_optional(db)
+        .await;
+
+    match resp {
+        Ok(resp) => match resp {
+            Some(user) => HttpResponse::Ok().json(User {
+                id: bigdecimal_to_u128!(user.id),
+                name: user.name,
+                guilds: None,
+                flags: 0,
+            }),
+            None => HttpResponse::NotFound().finish(),
+        },
+        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
+            reason: format!("database returned a error: {}", e),
+        }),
+    }
 }
