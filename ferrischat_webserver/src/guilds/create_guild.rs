1642f617b98c97476fdf1ff88cf8731c4a5e0eb6
0/0 <zero@imaskeleton.me>
2021-08-06 13:05:57 -0600

    add create guild endpoint

--- ferrischat_webserver/src/guilds/create_guild.rs
+++ ferrischat_webserver/src/guilds/create_guild.rs
@@ -1,6 +1,23 @@
 use actix_web::{HttpResponse, Responder};
+use sqlx::types::BigDecimal;
+use ferrischat_macros::{ get_db_or_fail};
+use ferrischat_snowflake_generator::generate_snowflake;
+use ferrischat_common::types::{Guild, InternalServerErrorJson};
+use crate::API_VERSION;
+use num_traits::FromPrimitive;
 
 /// POST /api/v1/guilds/
 pub async fn create_guild() -> impl Responder {
-    HttpResponse::Created().body("created guild test")
+    let db = get_db_or_fail!();
+    let guild_id = generate_snowflake::<0>(0, 0);
+    match sqlx::query!("INSERT INTO guilds VALUES ($1, $2, $3, 0, 0)", BigDecimal::from_u128(guild_id), 0, "New Guild").execute(db).await {
+        Ok(r) => {HttpResponse::Created().json(Guild {
+            id: guild_id,
+            owner_id: 0,
+            name: "New Guild".to_string(),
+            channels: None,
+            members: None
+        })}
+        Err(e) => {HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("DB returned a error: {}", e) })}
+    }
 }
