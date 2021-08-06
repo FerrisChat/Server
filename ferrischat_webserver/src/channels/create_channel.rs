08ab35d4544f76474ff7e69a8b172079d83cfca7
hydrostaticcog <hydro@hydrostaticcog.me>
2021-08-06 18:34:10 -0500

    add POST to channel and user endpoints

--- ferrischat_webserver/src/channels/create_channel.rs
+++ ferrischat_webserver/src/channels/create_channel.rs
@@ -1,6 +1,20 @@
 use actix_web::{HttpResponse, Responder};
+use sqlx::types::BigDecimal;
+use ferrischat_macros::{ get_db_or_fail};
+use ferrischat_snowflake_generator::generate_snowflake;
+use ferrischat_common::types::{Channel, InternalServerErrorJson};
+use crate::API_VERSION;
+use num_traits::FromPrimitive;
 
 /// POST /api/v1/channels/
 pub async fn create_channel() -> impl Responder {
-    HttpResponse::Created().body("created channel test")
+    let db = get_db_or_fail!();
+    let channel_id = generate_snowflake::<0>(0, 0);
+    match sqlx::query!("INSERT INTO channels VALUES ($1, $2)", BigDecimal::from_u128(channel_id), "New Channel").execute(db).await {
+        Ok(r) => {HttpResponse::Created().json(Channel {
+            id: channel_id,
+            name: "New Channel".to_string(),
+        })}
+        Err(e) => {HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("DB returned a error: {}", e) })}
+    }
 }
--- ferrischat_webserver/src/users/create_user.rs
+++ ferrischat_webserver/src/users/create_user.rs
@@ -1,6 +1,22 @@
 use actix_web::{HttpResponse, Responder};
+use sqlx::types::BigDecimal;
+use ferrischat_macros::{ get_db_or_fail};
+use ferrischat_snowflake_generator::generate_snowflake;
+use ferrischat_common::types::{User, InternalServerErrorJson};
+use crate::API_VERSION;
+use num_traits::FromPrimitive;
 
 /// POST /api/v1/users/
 pub async fn create_user() -> impl Responder {
-    HttpResponse::Created().body("created user test")
+    let db = get_db_or_fail!();
+    let user_id = generate_snowflake::<0>(0, 0);
+    match sqlx::query!("INSERT INTO users VALUES ($1, $2, null, $3)", BigDecimal::from_u128(user_id), "New User", 0).execute(db).await {
+        Ok(r) => {HttpResponse::Created().json(User {
+            id: user_id,
+            name: "New User".to_string(),
+            guilds: None,
+            flags: 0,
+        })}
+        Err(e) => {HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("DB returned a error: {}", e) })}
+    }
 }
