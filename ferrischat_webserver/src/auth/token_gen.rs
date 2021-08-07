ea0f5e92c27953a5d72e1acfec44baf5e9e7069b
0/0 <zero@imaskeleton.me>
2021-08-07 15:02:27 -0600

    change token to 256 bits

--- ferrischat_webserver/src/auth/token_gen.rs
+++ ferrischat_webserver/src/auth/token_gen.rs
@@ -4,7 +4,7 @@
 use ring::rand::SecureRandom;
 
 pub fn generate_random_bits() -> Option<Vec<u8>> {
-    let mut r = Vec::with_capacity(16);
+    let mut r = vec![0; 256];
     let rng = crate::RNG_CORE.get()?;
     rng.fill(&mut r).ok()?;
     Some(r)
