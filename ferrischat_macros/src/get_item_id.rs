af876f1a7d9746586a9c2fe8d008dbb11fad8637
0/0 <zero@imaskeleton.me>
2021-08-07 15:06:21 -0600

    add macro to fetch item ID

---
+++ ferrischat_macros/src/get_item_id.rs
@@ -0,0 +1,20 @@
+macro_rules! get_item_id {
+    ($req:expr, $name:expr) => {{
+        use actix_web::HttpResponse;
+        match $req.match_info().get($name) {
+            Some(id) => match id.parse() {
+                Ok(id) => id,
+                Err(e) => {
+                    return HttpResponse::BadRequest().json(InternalServerErrorJson {
+                        reason: format!("Failed to parse user ID as u128: {}", e),
+                    })
+                }
+            },
+            None => {
+                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
+                    reason: "User ID not found in match_info".to_string(),
+                })
+            }
+        }
+    }};
+}
