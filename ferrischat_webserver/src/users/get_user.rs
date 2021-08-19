c3fb363e82f19009d00270ef358e00f7ecd55f62
0/0 <zero@imaskeleton.me>
2021-08-19 08:23:46 -0600

    return guilds depending on if user is authorized

--- ferrischat_webserver/src/users/get_user.rs
+++ ferrischat_webserver/src/users/get_user.rs
@@ -1,24 +1,30 @@
-use actix_web::{web::Path, HttpResponse, Responder};
+use actix_web::{web::Path, HttpRequest, HttpResponse, Responder};
 use ferrischat_common::types::{InternalServerErrorJson, User};
 use ferrischat_macros::{bigdecimal_to_u128, get_db_or_fail};
 use num_traits::cast::ToPrimitive;
 use sqlx::types::BigDecimal;
 
 /// GET /api/v0/users/{user_id}
-pub async fn get_user(Path(user_id): Path<i64>, _: crate::Authorization) -> impl Responder {
+pub async fn get_user(req: HttpRequest, auth: crate::Authorization) -> impl Responder {
+    let user_id = get_item_id!(req, "user_id");
     let db = get_db_or_fail!();
-    let user_id = BigDecimal::from(user_id);
-    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", user_id)
+    let bigint_user_id = BigDecimal::from(user_id);
+    let authorized_user = auth.0;
+    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_user_id)
         .fetch_optional(db)
         .await;
 
     match resp {
         Ok(resp) => match resp {
             Some(user) => HttpResponse::Ok().json(User {
-                id: bigdecimal_to_u128!(user.id),
+                id: user_id,
                 name: user.name,
-                guilds: None,
-                flags: 0,
+                guilds: if authorized_user == user_id {
+                    user.guilds
+                } else {
+                    None
+                },
+                flags: user.flags,
             }),
             None => HttpResponse::NotFound().finish(),
         },
