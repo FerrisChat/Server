39a3234720c98a00c441628a812c01f4d70bbe3b
0/0 <zero@imaskeleton.me>
2021-08-20 01:32:46 -0600

    update get channel to support 128 bit ints

--- ferrischat_webserver/src/channels/get_channel.rs
+++ ferrischat_webserver/src/channels/get_channel.rs
@@ -1,13 +1,13 @@
-use actix_web::{web::Path, HttpResponse, Responder};
+use actix_web::{web::Path, HttpResponse, Responder, HttpRequest};
 use ferrischat_common::types::{Channel, InternalServerErrorJson};
 use ferrischat_macros::{bigdecimal_to_u128, get_db_or_fail};
 use num_traits::cast::ToPrimitive;
 use sqlx::types::BigDecimal;
 
 /// GET /api/v0/guilds/{guild_id/channels/{channel_id}
-pub async fn get_channel(Path(channel_id): Path<i64>, _: crate::Authorization) -> impl Responder {
+pub async fn get_channel(req: HttpRequest, _: crate::Authorization) -> impl Responder {
     let db = get_db_or_fail!();
-    let channel_id = BigDecimal::from(channel_id);
+    let channel_id = u128_to_bigdecimal!(get_item_id!(req, "guild_id"));
     let resp = sqlx::query!("SELECT * FROM channels WHERE id = $1", channel_id)
         .fetch_optional(db)
         .await;
