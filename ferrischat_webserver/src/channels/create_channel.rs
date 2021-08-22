f7007949c1b2f0d1ad906df27a960af1b524b224
0/0 <zero@imaskeleton.me>
2021-08-21 22:15:22 -0600

    add guild id to channel object on creation

--- ferrischat_webserver/src/channels/create_channel.rs
+++ ferrischat_webserver/src/channels/create_channel.rs
@@ -1,5 +1,5 @@
 use actix_web::web::Json;
-use actix_web::{HttpResponse, Responder};
+use actix_web::{HttpRequest, HttpResponse, Responder};
 use ferrischat_common::request_json::ChannelCreateJson;
 use ferrischat_common::types::{Channel, InternalServerErrorJson, ModelType};
 use ferrischat_macros::get_db_or_fail;
@@ -9,15 +9,23 @@ use ferrischat_snowflake_generator::generate_snowflake;
 pub async fn create_channel(
     _: crate::Authorization,
     channel_info: Json<ChannelCreateJson>,
+    req: HttpRequest,
 ) -> impl Responder {
     let db = get_db_or_fail!();
-    let channel_id = generate_snowflake::<0>(ModelType::Channel as u8, 0);
+
     let ChannelCreateJson { name } = channel_info.0;
+
+    let channel_id = generate_snowflake::<0>(ModelType::Channel as u8, 0);
     let bigint_channel_id = u128_to_bigdecimal!(channel_id);
+
+    let guild_id = get_item_id!(req, "guild_id");
+    let bigint_guild_id = u128_to_bigdecimal!(guild_id);
+
     match sqlx::query!(
-        "INSERT INTO channels VALUES ($1, $2)",
+        "INSERT INTO channels VALUES ($1, $2, $3)",
         bigint_channel_id,
-        name
+        name,
+        bigint_guild_id
     )
     .execute(db)
     .await
