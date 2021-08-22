db0baa39e673bafa3ba0e52c7f37d640c75a03d4
0/0 <zero@imaskeleton.me>
2021-08-21 22:37:24 -0600

    fix typo in get channel route

--- ferrischat_webserver/src/channels/get_channel.rs
+++ ferrischat_webserver/src/channels/get_channel.rs
@@ -4,7 +4,7 @@ use ferrischat_common::types::{Channel, InternalServerErrorJson, NotFoundJson};
 /// GET /api/v0/guilds/{guild_id/channels/{channel_id}
 pub async fn get_channel(req: HttpRequest, _: crate::Authorization) -> impl Responder {
     let db = get_db_or_fail!();
-    let channel_id = u128_to_bigdecimal!(get_item_id!(req, "guild_id"));
+    let channel_id = u128_to_bigdecimal!(get_item_id!(req, "channel_id"));
     let resp = sqlx::query!("SELECT * FROM channels WHERE id = $1", channel_id)
         .fetch_optional(db)
         .await;
