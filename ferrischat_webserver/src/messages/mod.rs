05e43746dc9a4c6f9df5ef320a84a9a473979006
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-26 22:32:44 -0700

    Fix that

--- ferrischat_webserver/src/messages/mod.rs
+++ ferrischat_webserver/src/messages/mod.rs
@@ -1,9 +1,9 @@
 mod delete_message;
 mod get_messages;
 mod send_message;
-mod get_message_history;
+mod message_history;
 
 pub use delete_message::*;
 pub use get_messages::*;
 pub use send_message::*;
-pub use get_message_history::*;
+pub use message_history::*;
