aba79ca24f5a57335f88d33c83349b4e5fc27c3f
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-27 10:15:40 -0700

    Implement edit channel

---
+++ ferrischat_webserver/src/channels/edit_channel.rs
@@ -0,0 +1,42 @@
+use actix_web::{HttpRequest, HttpResponse, Responder};
+use ferrischat_common::types::{InternalServerErrorJson, NotFoundJson, Channel};
+use ferrischat_common::request_json::ChannelUpdateJson;
+
+use num_traits::ToPrimitive;
+
+pub async fn edit_channel(req: HttpRequest, channel_info: Json<ChannelUpdateJson>, _: crate::Authorization) -> impl Responder {
+    let channel_id = get_item_id!(req, "channel_id");
+    let bigint_channel_id = u128_to_bigdecimal!(channel_id);
+
+    let db = get_db_or_fail!();
+
+    let ChannelCreateJson { name } = channel_info.0;
+
+    let resp = sqlx::query!("UPDATE channels SET name = $2 WHERE id = $1 RETURNING *", bigint_channel_id, name)
+        .fetch_optional(db)
+        .await;
+    
+    match resp {
+        Ok(resp) => match resp {
+            Some(channel) => HttpResponse::Ok().json(Channel {
+                id: channel.id
+                    .with_scale(0)
+                    .into_bigint_and_exponent()
+                    .0
+                    .to_u128(),
+                name: channel.name.clone(),
+                guild_id: channel.guild_id
+                    .with_scale(0)
+                    .into_bigint_and_exponent()
+                    .0
+                    .to_u128(),
+            })
+            None => HttpResponse::NotFound().json(NotFoundJson {
+                message: "Channel not found".to_string()
+            })
+        },
+        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
+            message: format!("DB returned an error: {}", e)
+        })
+    
+    }
