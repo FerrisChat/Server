297ecc2337c013dd51ef6b585c991dfd8aebfe88
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-08-27 16:27:35 -0400

    Create create_invite.rs

---
+++ ferrischat_webserver/src/invites/create_invite.rs
@@ -0,0 +1,11 @@
+use actix_web::web::Json;
+use actix_web::{HttpRequest, HttpResponse, Responder};
+use ferrischat_macros::get_db_or_fail;
+use ferrischat_snowflake_generator::generate_snowflake;
+
+// POST /api/v0/guilds/{guild_id}/invites
+pub async fn create_invite(
+
+) -> impl Responder {
+
+}
