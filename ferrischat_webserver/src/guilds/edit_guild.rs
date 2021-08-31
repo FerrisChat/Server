d0558800f7188eed4b75027b7cfd6b0099c7a5a2
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-08-30 22:12:28 -0400

    Add guard for when ``name`` is ``None``
    Pray that I got it correct because I need to sleep

--- ferrischat_webserver/src/guilds/edit_guild.rs
+++ ferrischat_webserver/src/guilds/edit_guild.rs
@@ -13,32 +13,44 @@ pub async fn edit_guild(
     let bigint_guild_id = u128_to_bigdecimal!(guild_id);
 
     let GuildUpdateJson { name } = guild_info.0;
-
-    let db = get_db_or_fail!();
-
-    let resp = sqlx::query!(
-        "UPDATE guilds SET name = $1 WHERE id = $2 RETURNING *",
-        name,
+    
+    sqlx::query!(
+        "SELECT * FROM guilds WHERE id = $1",
         bigint_guild_id
     )
     .fetch_optional(db)
     .await;
+    
+    match name {
+        Some(name) => {
+            let db = get_db_or_fail!();
 
-    match resp {
-        Ok(resp) => match resp {
-            Some(guild) => HttpResponse::Ok().json(Guild {
-                id: bigdecimal_to_u128!(guild.id),
-                owner_id: bigdecimal_to_u128!(guild.owner_id),
-                name: guild.name.clone(),
-                channels: None,
-                members: None,
-            }),
-            None => HttpResponse::NotFound().json(NotFoundJson {
-                message: "Guild not found".to_string(),
-            }),
+            let resp = sqlx::query!(
+                "UPDATE guilds SET name = $1 WHERE id = $2 RETURNING *",
+                name,
+                bigint_guild_id
+            )
+            .fetch_optional(db)
+            .await;
+
+            match resp {
+                Ok(resp) => match resp {
+                    Some(guild) => HttpResponse::Ok().json(Guild {
+                        id: bigdecimal_to_u128!(guild.id),
+                        owner_id: bigdecimal_to_u128!(guild.owner_id),
+                        name: guild.name.clone(),
+                        channels: None,
+                        members: None,
+                    }),
+                    None => HttpResponse::NotFound().json(NotFoundJson {
+                        message: "Guild not found".to_string(),
+                    }),
+                },
+                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
+                    reason: format!("DB returned an error: {}", e),
+                }),
+            }   
         },
-        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-            reason: format!("DB returned an error: {}", e),
-        }),
+        None => HttpResponse::BadRequest().finish()
     }
 }
