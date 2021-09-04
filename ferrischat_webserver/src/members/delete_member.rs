ab566026454ce9bb3490e31c8717fa77278a2d30
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-09-04 13:35:29 -0400

    Implement ``delete_member``

--- ferrischat_webserver/src/members/delete_member.rs
+++ ferrischat_webserver/src/members/delete_member.rs
@@ -2,5 +2,30 @@ use actix_web::{HttpRequest, HttpResponse, Responder};
 
 /// DELETE /api/v0/guilds/{guild_id}/members/{member_id}
 pub async fn delete_member(req: HttpRequest) -> impl Responder {
-    HttpResponse::NoContent()
+    let guild_id = {
+        let raw = get_item_id!(req, "guild_id");
+        u128_to_bigdecimal!(raw)
+    };
+    let member_id = {
+        let raw = get_item_id!(req, "member_id");
+        u128_to_bigdecimal!(raw)
+    };
+    
+    let db = get_db_or_fail!();
+
+    let resp = sqlx::query!("DELETE FROM members WHERE id = $1 AND guild_id = $2 RETURNING (id)", member_id, guild_id)
+        .fetch_optional(db)
+        .await;
+
+    match resp {
+        Ok(r) => match r {
+            Some(_) => HttpResponse::NoContent().finish(),
+            None => HttpResponse::NotFound().json(NotFoundJson {
+                message: "Member not found".to_string(),
+            }),
+        },
+        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
+            reason: format!("Database responded with an error: {}", e)
+        }),
+    }
 }
