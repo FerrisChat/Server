0ff5d9e036804a63cc0ef9dbf692d309b1afd8e0
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-10-22 22:30:26 -0700

    Implement MessageUpdate event

--- ferrischat_webserver/src/messages/edit_message.rs
+++ ferrischat_webserver/src/messages/edit_message.rs
@@ -1,3 +1,7 @@
+use crate::ws::{fire_event, WsEventError};
+
+use ferrischat_common::ws::WsOutboundEvent;
+
 use actix_web::web::Json;
 
 use actix_web::{HttpRequest, HttpResponse, Responder};
@@ -16,6 +20,25 @@ pub async fn edit_message(
     let bigint_message_id = u128_to_bigdecimal!(message_id);
 
     let db = get_db_or_fail!();
+
+    let guild_id = {
+        let resp = sqlx::query!(
+            "SELECT guild_id FROM channels WHERE id = $1",
+            bigint_channel_id
+        )
+        .fetch_one(db)
+        .await;
+
+        match resp {
+            Ok(r) => bigdecimal_to_u128!(r.guild_id),
+            Err(e) => {
+                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
+                    reason: format!("DB returned a error: {}", e),
+                })
+            }
+        }
+    };
+
     {
         let resp = sqlx::query!(
             "SELECT author_id FROM messages WHERE channel_id = $1 AND id = $2",
@@ -54,15 +77,8 @@ pub async fn edit_message(
 
     match resp {
         Ok(resp) => match resp {
-            Some(message) => HttpResponse::Ok().json(Message {
-                id: message_id,
-                channel_id: channel_id,
-                author_id: bigdecimal_to_u128!(message.author_id),
-                content: message.content,
-                edited_at: message.edited_at,
-                embeds: vec![],
-            }),
-            None => HttpResponse::NotFound().json(NotFoundJson {
+            Some(message) => (),
+            None => return HttpResponse::NotFound().json(NotFoundJson {
                 message: "Message not found".to_string(),
             }),
         },
@@ -70,4 +86,30 @@ pub async fn edit_message(
             reason: format!("DB returned an error: {}", e),
         }),
     }
+
+    let msg_obj = Message {
+        id: message_id,
+        channel_id: channel_id,
+        author_id: bigdecimal_to_u128!(message.author_id),
+        content: message.content,
+        edited_at: message.edited_at,
+        embeds: vec![],
+    };
+
+    let event = WsOutboundEvent::MessageUpdate {
+        message: msg_obj.clone(),
+    };
+
+    if let Err(e) = fire_event(format!("message_{}_{}", channel_id, guild_id), &event).await {
+        let reason = match e {
+            WsEventError::MissingRedis => "Redis pool missing".to_string(),
+            WsEventError::RedisError(e) => format!("Redis returned an error: {}", e),
+            WsEventError::JsonError(e) => {
+                format!("Failed to serialize message to JSON format: {}", e)
+            }
+        };
+        return HttpResponse::InternalServerError().json(InternalServerErrorJson { reason });
+    }
+
+    HttpResponse::Ok().json(msg_obj)
 }
