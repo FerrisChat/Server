2754051853f3b94179dde6917111c14b78a2e5ea
0/0 <zero@imaskeleton.me>
2021-10-23 19:59:27 -0600

    bail if `pclmulqdq` not supported

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use ferrischat_webserver::entrypoint;
 
 #[actix_web::main]
 async fn main() {
+    #[cfg(target_arch = "x86_64")]
+    if !is_x86_feature_detected!("pclmulqdq") {
+        eprintln!("Your CPU doesn't support `pclmulqdq`. Exiting.");
+        std::process::abort()
+    }
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     entrypoint().await;
