98cf7195c75d8e26dc5f85c81e9380eae890a37d
0/0 <zero@imaskeleton.me>
2021-11-02 20:07:19 -0600

    update role migration

--- migrations/20211102233704_roles.sql
+++ migrations/20211102233704_roles.sql
@@ -1,3 +1,15 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXISTS roles (id numeric(39) PRIMARY KEY, name VARCHAR(256), color INT, position SMALLINT, parent_guild numeric(39) REFERENCES guilds ON DELETE CASCADE);
-CREATE TABLE IF NOT EXISTS role_data (internal_id numeric(39) PRIMARY KEY, guild_id numeric(39) REFERENCES guilds ON DELETE CASCADE, user_id numeric(39) REFERENCES members ON DELETE CASCADE, role_id numeric(39) REFERENCES roles ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS roles (
+    id numeric(39) PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    color INT,
+    position SMALLINT NOT NULL DEFAULT 0,
+    permissions BIGINT NOT NULL DEFAULT 0,
+    parent_guild numeric(39) REFERENCES guilds ON DELETE CASCADE NOT NULL
+);
+CREATE TABLE IF NOT EXISTS role_data (
+    internal_id numeric(39) PRIMARY KEY NOT NULL,
+    guild_id numeric(39) REFERENCES guilds ON DELETE CASCADE NOT NULL,
+    user_id numeric(39) REFERENCES members ON DELETE CASCADE NOT NULL,
+    role_id numeric(39) REFERENCES roles ON DELETE CASCADE NOT NULL
+);
