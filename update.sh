72f8ed59f3832cbece4b42ee3a292346aa287f9e
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-04 19:11:56 -0500

    fix bin update

--- update.sh
+++ update.sh
@@ -8,24 +8,12 @@ else
   cd /tmp/fc_setup
 fi
 
-echo "Checking if Rust is installed..."
-if [[ ! $(command -v cargo &> /dev/null) ]]; then
-  echo "cargo not found, installing it now..."
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y --default-toolchain nightly
-  source $HOME/.cargo/env
-fi
+echo "Downloading new binary..."
+wget -O "ferrischat_server" https://download.ferris.chat/FerrisChat_Server
 
-
-echo "Cloning server repo..."
-git clone --branch develop https://github.com/FerrisChat/Server || (cd Server; git checkout develop; git pull; cd ..)
-
-
-cd Server
-echo "Building server binary with optimizations..."
-SQLX_OFFLINE="true" RUSTFLAGS="-Ctarget-cpu=native --emit=asm" cargo build --release
 echo "Copying server binary to /usr/bin..."
 rm /usr/bin/ferrischat_server
-mv target/release/ferrischat_server /usr/bin
+mv ferrischat_server /usr/bin
 
 echo "Restarting systemd service..."
 systemctl restart ferrischat_server
