ea4906709faa55f4664c3cb6e8a073f84a033338
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-12 21:37:22 -0600

    Revert "Add full author data to messages"
    This reverts commit 5b251c80

--- ferrischat_webserver/src/messages/message_history.rs
+++ ferrischat_webserver/src/messages/message_history.rs
@@ -1,9 +1,7 @@
 use actix_web::{web::Query, HttpRequest, HttpResponse, Responder};
 
 use ferrischat_common::request_json::GetMessageHistoryParams;
-use ferrischat_common::types::{
-    BadRequestJson, InternalServerErrorJson, Message, MessageHistory, User,
-};
+use ferrischat_common::types::{BadRequestJson, InternalServerErrorJson, Message, MessageHistory};
 
 use num_traits::ToPrimitive;
 
@@ -32,9 +30,7 @@ pub async fn get_message_history(
 
     let messages = {
         let resp = sqlx::query!(
-            r#"SELECT m.*, (
-                SELECT u.* FROM users u WHERE id = m.author_id
-            ) AS author FROM messages m WHERE channel_id = $1 LIMIT $2"#,
+            "SELECT * FROM messages WHERE channel_id = $1 LIMIT $2",
             bigint_channel_id,
             limit
         )
@@ -59,14 +55,6 @@ pub async fn get_message_history(
                             .into_bigint_and_exponent()
                             .0
                             .to_u128()?,
-                        author: Some(User {
-                            id: bigdecimal_to_u128!(x.author_id),
-                            name: x.author.name,
-                            avatar: None,
-                            guilds: None,
-                            discriminator: x.author.discriminator,
-                            flags: x.author.flags,
-                        }),
                         edited_at: x.edited_at,
                         embeds: vec![],
                     })
