5f44555172705db75b9bd952972409db2dbf519c
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-13 19:57:08 -0600

    Revert "update websocket URL"
    This reverts commit ae97c708f19bf37d3c61a07ba3d0d918ddd7b7c0.

--- ferrischat_webserver/src/ws/info.rs
+++ ferrischat_webserver/src/ws/info.rs
@@ -4,6 +4,6 @@ use ferrischat_common::types::WsConnectionInfo;
 /// GET /api/v0/ws/info
 pub async fn ws_info(_: crate::Authorization) -> impl Responder {
     HttpResponse::Ok().json(WsConnectionInfo {
-        url: "wss://api.ferris.chat:8081".to_string(),
+        url: "wss://api.ferris.chat/v0/ws/connect".to_string(),
     })
 }
