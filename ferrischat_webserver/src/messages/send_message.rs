ad50ecca9303e5bbdc869726c0e9ad251420c00c
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-13 16:19:55 -0800

    Update send_message.rs

--- ferrischat_webserver/src/messages/send_message.rs
+++ ferrischat_webserver/src/messages/send_message.rs
@@ -12,7 +12,8 @@ pub async fn create_message(
     req: HttpRequest,
     json: Json<MessageCreateJson>,
 ) -> impl Responder {
-    let content = json.content.clone();
+    let MessageCreateJson { content, nonce } = json.0;
+
     if content.len() > 10240 {
         return HttpResponse::BadRequest().json(BadRequestJson {
             reason: "message content size must be fewer than 10,240 bytes".to_string(),
@@ -73,6 +74,7 @@ pub async fn create_message(
         author: None,
         edited_at: None,
         embeds: vec![],
+        nonce: nonce,
     };
 
     let event = WsOutboundEvent::MessageCreate {
