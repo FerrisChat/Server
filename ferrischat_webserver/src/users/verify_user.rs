aecf7b68388c0fb72d765581cea27b21b45f2ce0
valkyrie_pilot <valk@randomairborne.dev>
2021-11-15 23:23:47 -0700

    A really horrible commit that adds full verification support.

--- ferrischat_webserver/src/users/verify_user.rs
+++ ferrischat_webserver/src/users/verify_user.rs
@@ -9,153 +9,24 @@ use simd_json::ValueAccess;
 use sqlx::Error;
 
 use ferrischat_redis::{redis::AsyncCommands, REDIS_MANAGER};
-
 /// POST /v0/verify
-pub async fn send_verification_email(
-    req: HttpRequest,
-    auth: crate::Authorization,
-) -> impl Responder {
-    let authorized_user = auth.0;
-    match sqlx::query!("SELECT email FROM users WHERE id = $1", authorized_user)
-        .fetch_optional(db)
-        .await
-    {
-        Ok(email) => {
-            let user_email = email.email;
-        }
-        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-            reason: format!("Database returned a error: {}", e),
-        }),
-    }
-    let checker_input = CheckEmailInput::new(vec![user_email.into()]);
-    let checked_email = check_email(&checker_input).await;
-    if checked_email[0].syntax.is_valid_syntax {
-        if checked_email[0].is_reachable == Reachable::Safe
-            || checked_email[0].is_reachable == Reachable::Risky
-            || checked_email[0].is_reachable == Reachable::Unknown
-        {
+pub async fn send_verification_email(req: HttpRequest,auth: crate::Authorization) -> impl Responder { let authorized_user = auth.0;let user_email = match sqlx::query!("SELECT email FROM users WHERE id = $1", authorized_user).fetch_optional(db).await { Ok(email) => email.email, Err(e) => { return HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("Database returned a error: {}", e), }) } };let checker_input = CheckEmailInput::new(vec![user_email.into()]);let checked_email = check_email(&checker_input).await;if checked_email[0].syntax.is_valid_syntax { if checked_email[0].is_reachable == Reachable::Safe || checked_email[0].is_reachable == Reachable::Risky || checked_email[0].is_reachable == Reachable::Unknown {
             // get configs
-            let mut redis = REDIS_MANAGER
-                .get()
-                .expect("redis not initialized: call load_redis before this")
-                .clone();
-            match host = redis.get("config:email:host").get::<String, String>().await {
-                Ok(_) => password,
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("No SMTP server host set."),
-                }),
-            }
-            match host = redis
-                .get("config:email:username")
-                .get::<String, String>()
-                .await
-            {
-                Ok(_) => password,
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("No SMTP server username set."),
-                }),
-            }
-            match password = redis
-                .get("config:email:password")
-                .get::<String, String>()
-                .await
-            {
-                Ok(_) => password,
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("No SMTP server password set."),
-                }),
-            }
-            let token = match ferrischat_webserver::auth::generate_random_bits() {
-                Some(b) => base64::encode_config(b, base64::URL_SAFE),
-                None => {
-                    return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                        reason: "failed to generate random bits for token generation".to_string(),
-                    })
-                }
-            };
-            match message = Message::builder()
-                .from("Ferris <verification@ferris.chat>".parse().unwrap())
-                .reply_to("Ferris <hello@ferris.chat>".parse().unwrap())
-                .to(user_email.parse().unwrap())
-                .subject("FerrisChat Email Verification")
-                .body(String::from(format!(
-                    "Welcome to FerrisChat!<br><a href=\"https://api.ferris.chat/v0/verify/{}\">Click here to verify \
+            let mut redis = REDIS_MANAGER.get().expect("redis not initialized: call load_redis before this").clone();match host = redis.get::<String, String>("config:email:host".to_string()).await { Ok(_) => password, Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("No SMTP server host set."), }), }match host = redis.get::<String, String>("config:email:username".to_string()).await { Ok(_) => password, Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("No SMTP server username set."), }), }match password = redis.get::<String, String>("config:email:password".to_string()).await { Ok(_) => password, Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("No SMTP server password set."), }), }let token = match ferrischat_webserver::auth::generate_random_bits() { Some(b) => base64::encode_config(b, base64::URL_SAFE), None => { return HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: "failed to generate random bits for token generation".to_string(), }); } };match message = Message::builder().from("Ferris <verification@ferris.chat>".parse().unwrap()).reply_to("Ferris <hello@ferris.chat>".parse().unwrap()).to(user_email.parse().unwrap()).subject("FerrisChat Email Verification").body(String::from(format!("Welcome to FerrisChat!<br><a href=\"https://api.ferris.chat/v0/verify/{}\">Click here to verify \
         your email!</a> (expires in 1 hour) <br><br> If you don't know what this is, reset your token and change \
-        your password ASAP.",
-                    token
-                ))) {
-                Ok(_) => message,
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!(
-                        "This should not have happened. Submit a bug report on \
-                    https://github.com/ferrischat/server/issues with the error `{}`",
-                        e
-                    ),
-                }),
-            }
+        your password ASAP.", token))) { Ok(_) => message, Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("This should not have happened. Submit a bug report on \
+                    https://github.com/ferrischat/server/issues with the error `{}`", e), }), }
 
             let mail_creds = Credentials::new(username.to_string(), password.to_string());
 
-            // Open a remote connection to gmail
-            match mailer = SmtpTransport::relay(host.as_str().unwrap())
-                .credentials(mail_creds)
-                .build()
-            {
-                Ok(_) => mailer,
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!(
-                        "Error creating SMTP transport! Please submit a bug report on \
-                    https://github.com/ferrischat/server/issues with the error `{}`",
-                        e
-                    ),
-                }),
-            }
+            // Open a remote connection to the mailserver
+            match mailer = SmtpTransport::relay(host.as_str().unwrap()).credentials(mail_creds).build() { Ok(_) => mailer, Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("Error creating SMTP transport! Please submit a bug report on \
+                    https://github.com/ferrischat/server/issues with the error `{}`", e), }), }
 
             // Send the email
-            match mailer.send(&message) {
-                Ok(_) => HttpResponse::Ok().finish(),
-                Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!(
-                        "Mailer failed to send correctly! Please submit a bug report \
-                    on https://github.com/ferrischat/server/issues with the error `{}`",
-                        e
-                    ),
-                }),
-            }
+            match mailer.send(&message) { Ok(_) => HttpResponse::Ok().finish(), Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson { reason: format!("Mailer failed to send correctly! Please submit a bug report \
+                    on https://github.com/ferrischat/server/issues with the error `{}`", e), }),}
             // writes the token to redis
-            let r = redis
-                .set_ex::<String, String, String>(
-                    format!("email:tokens:{}", token),
-                    user_email,
-                    86400,
-                )
-                .await;
-        } else {
-            HttpResponse::Conflict().json(InternalServerErrorJson {
-                reason: format!("Email deemed unsafe to send to. Is it a real email?"),
-            })
-        }
-    } else {
-        HttpResponse::Conflict().json(InternalServerErrorJson {
-            reason: format!("Email {} is invalid.", user_email),
-        })
-    }
-}
-
+            let r = redis.set_ex::<String, String, String>(format!("email:tokens:{}", token), user_email, 86400,).await;}else{HttpResponse::Conflict().json(InternalServerErrorJson {reason: format!("Email deemed unsafe to send to. Is it a real email?"),})}}else{HttpResponse::Conflict().json(InternalServerErrorJson {reason: format!("Email {} is invalid.", user_email),})}}
 /// GET /v0/verify/{token}
-pub async fn verify_email(req: HttpRequest, path: actix_web::web::Path<String>) -> impl Responder {
-    let mut redis = REDIS_MANAGER
-        .get()
-        .expect("redis not initialized: call load_redis before this")
-        .clone();
-    match host = redis
-        .get(format!("email:tokens:{}", path.into_inner()))
-        .get::<String, String>()
-        .await
-    {
-        Ok(_) => HttpResponse::Ok().body("Verified email. You can close this page."),
-        Err(e) => HttpResponse::NotFound().json(NotFoundJson {
-            message: format!("This token has expired or was not found.")
-        }),
-    }
-}
+pub async fn verify_email(req: HttpRequest, path: actix_web::web::Path<String>) -> impl Responder{let mut redis = REDIS_MANAGER.get().expect("redis not initialized: call load_redis before this").clone();match host = redis.get(format!("email:tokens:{}",path.into_inner())).get::<String,String>().await{Ok(_)=>HttpResponse::Ok().body("Verified email. You can close this page."),Err(_)=>HttpResponse::NotFound().json(NotFoundJson{message: format!("This token has expired or was not found."),}),}if let Err(e) = sqlx::query!("UPDATE users SET verified = true WHERE email = $1",authorized_user).fetch_optional(db).await{HttpResponse::InternalServerError().json(InternalServerErrorJson {reason: format!("Database returned a error: {}", e), }) } }
