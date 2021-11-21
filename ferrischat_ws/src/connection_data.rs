0761dd7c9c98f5abc639f337571823203be6fae8
0/0 <zero@imaskeleton.me>
2021-11-20 20:38:28 -0700

    fix typo

--- ferrischat_ws/src/connection_data.rs
+++ ferrischat_ws/src/connection_data.rs
@@ -2,6 +2,6 @@ use std::sync::atomic::AtomicBool;
 use uuid::Uuid;
 
 pub struct WebSocketConnectionData {
-    pub identify_recieved: AtomicBool,
+    pub identify_received: AtomicBool,
     pub connection_id: Uuid,
 }
