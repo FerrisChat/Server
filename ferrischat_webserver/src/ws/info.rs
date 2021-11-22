4b5decd49b440e32aef7dca375f83c3a58db3777
0/0 <zero@imaskeleton.me>
2021-11-21 21:09:23 -0700

    update WS API URL

--- ferrischat_webserver/src/ws/info.rs
+++ ferrischat_webserver/src/ws/info.rs
@@ -5,6 +5,6 @@ use ferrischat_common::types::WsConnectionInfo;
 /// GET /api/v0/ws/info
 pub async fn ws_info(_: crate::Authorization) -> impl Responder {
     HttpResponse::Ok().json(WsConnectionInfo {
-        url: "wss://api.ferris.chat:8081".to_string(),
+        url: "wss://ws.api.ferris.chat".to_string(),
     })
 }
