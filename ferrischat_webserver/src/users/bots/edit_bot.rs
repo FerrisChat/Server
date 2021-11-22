a15aba044b391278ad7374fa82a6000b7a03b883
0/0 <zero@imaskeleton.me>
2021-11-22 10:28:30 -0700

    fix bugs in bot edit

--- ferrischat_webserver/src/users/bots/edit_bot.rs
+++ ferrischat_webserver/src/users/bots/edit_bot.rs
@@ -3,6 +3,7 @@ use actix_web::web::Json;
 use actix_web::{HttpRequest, HttpResponse, Responder};
 use ferrischat_common::request_json::BotUpdateJson;
 use ferrischat_common::types::{InternalServerErrorJson, NotFoundJson, User, UserFlags};
+use sqlx::types::BigDecimal;
 
 /// PATCH `/api/v0/users/{user_id}/bots/{bot_id}`
 /// Edits the bot with the attached payload
@@ -11,29 +12,34 @@ pub async fn edit_bot(
     bot_info: Json<BotUpdateJson>,
     auth: crate::Authorization,
 ) -> impl Responder {
-    let user_id = get_item_id!(req, "bot_id");
-    let bigdecimal_user_id = u128_to_bigdecimal!(user_id);
-
-    let bigint_user_id = u128_to_bigdecimal!(user_id);
+    let bot_id = get_item_id!(req, "bot_id");
+    let bigint_bot_id = u128_to_bigdecimal!(bot_id);
 
     let BotUpdateJson { username } = bot_info.0;
 
     let db = get_db_or_fail!();
 
-    let bigdecimal_owner_id =
-        match sqlx::query!("SELECT * FROM bots WHERE user_id = $1", bigdecimal_user_id,)
-            .fetch_one(db)
-            .await
-        {
-            Ok(r) => r.owner_id,
-            Err(e) => {
-                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("DB returned a error: {}", e),
-                    is_bug: false,
-                    link: None,
-                })
-            }
-        };
+    let bigdecimal_owner_id: BigDecimal = match sqlx::query!(
+        "SELECT owner_id FROM bots WHERE user_id = $1",
+        bigint_bot_id,
+    )
+    .fetch_optional(db)
+    .await
+    {
+        Ok(Some(r)) => r.owner_id,
+        Ok(None) => {
+            return HttpResponse::NotFound().json(NotFoundJson {
+                message: format!("Unknown bot with id {}", bot_id),
+            })
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
+                reason: format!("DB returned a error: {}", e),
+                is_bug: false,
+                link: None,
+            })
+        }
+    };
 
     let owner_id = bigdecimal_to_u128!(bigdecimal_owner_id);
 
@@ -45,7 +51,7 @@ pub async fn edit_bot(
         if let Err(e) = sqlx::query!(
             "UPDATE users SET name = $1 WHERE id = $2",
             username,
-            bigint_user_id
+            bigint_bot_id
         )
         .execute(db)
         .await
@@ -58,13 +64,13 @@ pub async fn edit_bot(
         }
     }
 
-    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_user_id)
+    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_bot_id)
         .fetch_optional(db)
         .await;
 
     match resp {
         Ok(Some(user)) => HttpResponse::Ok().json(User {
-            id: user_id,
+            id: bot_id,
             name: user.name.clone(),
             avatar: None,
             guilds: None,
@@ -72,7 +78,7 @@ pub async fn edit_bot(
             discriminator: user.discriminator,
         }),
         Ok(None) => HttpResponse::NotFound().json(NotFoundJson {
-            message: format!("Unknown user with id {}", user_id),
+            message: format!("Unknown bot with id {}", bot_id),
         }),
         Err(e) => {
             return HttpResponse::InternalServerError().json(InternalServerErrorJson {
