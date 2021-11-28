ed0cbef10b14a9431922bc3f05f3ff53a39625d3
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-28 12:36:23 -0600

    rewrite get_role, fmt
    Signed-off-by: hydrostaticcog <hydro@hydrostaticcog.me>

--- ferrischat_webserver/src/guilds/roles/get_role.rs
+++ ferrischat_webserver/src/guilds/roles/get_role.rs
@@ -1,33 +1,39 @@
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use crate::WebServerError;
+use axum::extract::Path;
 use ferrischat_common::perms::Permissions;
-use ferrischat_common::types::{InternalServerErrorJson, NotFoundJson, Role};
+use ferrischat_common::types::{NotFoundJson, Role};
+use serde::Serialize;
 
 /// GET `/api/v0/guilds/{guild_id/roles/{role_id}`
-pub async fn get_role(req: HttpRequest, _: crate::Authorization) -> impl Responder {
-    let db = get_db_or_fail!();
-    let role_id = u128_to_bigdecimal!(get_item_id!(req, "role_id"));
-    let resp = sqlx::query!("SELECT * FROM roles WHERE id = $1", role_id)
-        .fetch_optional(db)
-        .await;
-
-    match resp {
-        Ok(resp) => match resp {
-            Some(role) => HttpResponse::Ok().json(Role {
-                id: bigdecimal_to_u128!(role.id),
-                name: role.name,
-                color: role.color,
-                position: role.position,
-                guild_id: bigdecimal_to_u128!(role.parent_guild),
-                permissions: Permissions::from_bits_truncate(role.permissions),
-            }),
-            None => HttpResponse::NotFound().json(NotFoundJson {
-                message: format!("Unknown role with id {}", role_id),
-            }),
-        },
-        Err(e) => HttpResponse::InternalServerError().json(InternalServerErrorJson {
-            reason: format!("database returned a error: {}", e),
-            is_bug: false,
-            link: None,
-        }),
-    }
+pub async fn get_role(
+    Path(role_id): Path<u128>,
+    _: crate::Authorization,
+) -> Result<crate::Json<Role>, WebServerError<impl Serialize>> {
+    let bigint_role_id = u128_to_bigdecimal!(role_id);
+    Ok(
+        sqlx::query!("SELECT * FROM roles where id = $1", bigint_role_id)
+            .fetch_optional(get_db_or_fail!())
+            .await
+            .map_err(|e| WebServerError::Database(e))?
+            .map(|r| crate::Json {
+                obj: Role {
+                    id: role_id,
+                    name: r.name,
+                    color: r.color,
+                    position: r.position,
+                    guild_id: bigdecimal_to_u128!(r.parent_guild),
+                    permissions: Permissions::from_bits_truncate(r.permissions),
+                },
+                code: 200,
+            })
+            .ok_or_else(|| {
+                (
+                    404,
+                    NotFoundJson {
+                        message: format!("Unknown role with ID {}", role_id),
+                    },
+                )
+                    .into()
+            })?,
+    )
 }
