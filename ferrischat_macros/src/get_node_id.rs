f68cb2488daf33d610633cebee4c70794ab51595
0/0 <zero@imaskeleton.me>
2021-11-27 19:06:56 -0700

    rewrite node ID conversion that I missed
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_macros/src/get_node_id.rs
+++ ferrischat_macros/src/get_node_id.rs
@@ -2,21 +2,10 @@
 /// Expands to a block that either fetches the current node ID and returns it or
 /// returns a HTTP 500 from the function.
 macro_rules! get_node_id {
-    () => {{
-        use ferrischat_common::types::InternalServerErrorJson;
-        use ferrischat_redis::NODE_ID;
-        match NODE_ID.get() {
-                Some(id) => *id,
-                None => {
-                    return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                        reason: "Redis has not been set up yet".to_string(),
-                        is_bug: true,
-                        link: Some(
-                    "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&labels=bug&\
-                    template=api_bug_report.yml&title=%5B500%5D%3A+redis+not+set+up"
-                        .to_string()),
-                    })
-                }
-            }
-    }};
+    () => {
+        ferrischat_redis::NODE_ID
+            .get()
+            .map(|i| *i)
+            .ok_or(WebServerError::MissingNodeId)?
+    };
 }
