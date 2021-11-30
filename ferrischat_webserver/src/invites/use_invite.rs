a1798758113aea08fa34ed491b0da57f9179f506
0/0 <zero@imaskeleton.me>
2021-11-30 14:07:31 -0700

    fix use_invite.rs with 2.3x less code :P
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/invites/use_invite.rs
+++ ferrischat_webserver/src/invites/use_invite.rs
@@ -1,336 +1,144 @@
 use crate::ws::{fire_event, WsEventError};
-use ferrischat_common::ws::WsOutboundEvent;
-
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use crate::WebServerError;
+use axum::extract::Path;
 use ferrischat_common::types::{
     BadRequestJson, InternalServerErrorJson, Invite, Json, Member, NotFoundJson, User, UserFlags,
 };
+use ferrischat_common::ws::WsOutboundEvent;
+use serde::Serialize;
 use sqlx::types::time::OffsetDateTime;
+use sqlx::types::BigDecimal;
 
 const FERRIS_EPOCH: i64 = 1_577_836_800_000;
 
-pub async fn use_invite(req: HttpRequest, auth: crate::Authorization) -> impl Responder {
-    let invite_code = {
-        match req.match_info().get("code") {
-                Some(invite_code) => match invite_code.parse::<String>() {
-                    Ok(invite_code) => invite_code,
-                    Err(_) => return HttpResponse::BadRequest().json(BadRequestJson {
-                        reason: "Failed to parse invite code as String".to_string(),
-                        location: None
-                    }),
-                },
-                None => {
-                    return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                        reason: "code not found in match_info: this is a bug, please report it"
-                            .to_string(),
-                        is_bug: true,
-                        link: Some(
-                            "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
-                        labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+code+not+found+in+match_info"
-                                .to_string(),
-                        ),
-                    })
-                }
-            }
-    };
-
-    let user_id = auth.0;
+pub async fn use_invite(
+    Path(invite_code): Path<String>,
+    crate::Authorization(user_id): crate::Authorization,
+) -> Result<crate::Json<impl Serialize>, WebServerError<impl Serialize>> {
     let bigint_user_id = u128_to_bigdecimal!(user_id);
 
     let db = get_db_or_fail!();
 
-    let resp = sqlx::query!("SELECT * FROM invites WHERE code = $1", invite_code)
+    let invite = sqlx::query!("SELECT * FROM invites WHERE code = $1", invite_code)
         .fetch_optional(db)
-        .await;
+        .await?
+        .map(|x| bigdecimal_to_u128!(x))
+        .ok_or_else(|| {
+            (
+                404,
+                NotFoundJson {
+                    message: format!("Unknown invite with code {}", invite_code),
+                },
+            )
+        })?;
 
-    let guild_id = {
-        match resp {
-            Ok(ref resp) => match resp {
-                Some(invite) => bigdecimal_to_u128!(invite.guild_id),
-                None => {
-                    return HttpResponse::NotFound().json(NotFoundJson {
-                        message: format!("Unknown invite with code {}", invite_code),
-                    })
-                }
-            },
-            Err(e) => {
-                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("DB returned an error: {}", e),
-                    is_bug: false,
-                    link: None,
-                })
-            }
+    let bigint_guild_id: BigDecimal = invite.guild_id;
+    let guild_id = bigdecimal_to_u128!(bigint_guild_id);
+    let uses = invite.uses + 1;
+    let unix_timestamp = OffsetDateTime::now_utc().unix_timestamp();
+    let now = unix_timestamp - FERRIS_EPOCH;
+    let mut to_delete = false;
+    if let Some(max_uses) = invite.max_uses {
+        if uses > max_uses.into() {
+            to_delete = true;
         }
-    };
-
-    let member_obj = match resp {
-        Ok(resp) => match resp {
-            Some(invite) => {
-                let uses = invite.uses + 1;
-                let unix_timestamp = OffsetDateTime::now_utc().unix_timestamp();
-                let now = unix_timestamp - FERRIS_EPOCH;
-                if let Some(max_uses) = invite.max_uses {
-                    if uses > max_uses.into() {
-                        let delete_resp =
-                            sqlx::query!("DELETE FROM invites WHERE code = $1", invite_code)
-                                .execute(db)
-                                .await;
-
-                        return match delete_resp {
-                            Ok(_) => {
-                                let invite_obj = Invite {
-                                    code: invite.code.clone(),
-                                    owner_id: bigdecimal_to_u128!(invite.owner_id),
-                                    guild_id,
-                                    created_at: invite.created_at,
-                                    uses,
-                                    max_uses: invite.max_uses,
-                                    max_age: invite.max_age,
-                                };
-
-                                let event = WsOutboundEvent::InviteDelete { invite: invite_obj };
-
-                                if let Err(e) =
-                                    fire_event(format!("invite_{}", guild_id), &event).await
-                                {
-                                    let reason = match e {
-                                        WsEventError::MissingRedis => {
-                                            "Redis pool missing".to_string()
-                                        }
-                                        WsEventError::RedisError(e) => {
-                                            format!("Redis returned an error: {}", e)
-                                        }
-                                        WsEventError::JsonError(e) => {
-                                            format!(
-                                                "Failed to serialize message to JSON format: {}",
-                                                e
-                                            )
-                                        }
-                                        WsEventError::PoolError(e) => {
-                                            format!("`deadpool` returned an error: {}", e)
-                                        }
-                                    };
-                                    HttpResponse::InternalServerError()
-                                        .json(InternalServerErrorJson {
-                                            reason,
-                                            is_bug: true,
-                                            link: Some(
-                                                "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
-                                                labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+fire+event"
-                                                    .to_string(),
-                                            )})
-                                } else {
-                                    HttpResponse::Gone().finish()
-                                }
-                            }
-                            Err(e) => {
-                                HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                                    reason: format!("DB returned an error: {}", e),
-                                    is_bug: false,
-                                    link: None,
-                                })
-                            }
-                        };
-                    }
-                };
-
-                if let Some(max_age) = invite.max_age {
-                    if (now - invite.created_at) > max_age {
-                        let delete_resp =
-                            sqlx::query!("DELETE FROM invites WHERE code = $1", invite_code)
-                                .execute(db)
-                                .await;
-
-                        return match delete_resp {
-                            Ok(_) => {
-                                let invite_obj = Invite {
-                                    code: invite.code.clone(),
-                                    owner_id: bigdecimal_to_u128!(invite.owner_id),
-                                    guild_id,
-                                    created_at: invite.created_at,
-                                    uses: invite.uses,
-                                    max_uses: invite.max_uses,
-                                    max_age: Some(max_age),
-                                };
-
-                                let event = WsOutboundEvent::InviteDelete { invite: invite_obj };
-
-                                if let Err(e) =
-                                    fire_event(format!("invite_{}", guild_id), &event).await
-                                {
-                                    let reason = match e {
-                                        WsEventError::MissingRedis => {
-                                            "Redis pool missing".to_string()
-                                        }
-                                        WsEventError::RedisError(e) => {
-                                            format!("Redis returned an error: {}", e)
-                                        }
-                                        WsEventError::JsonError(e) => {
-                                            format!(
-                                                "Failed to serialize message to JSON format: {}",
-                                                e
-                                            )
-                                        }
-                                        WsEventError::PoolError(e) => {
-                                            format!("`deadpool` returned an error: {}", e)
-                                        }
-                                    };
-                                    HttpResponse::InternalServerError()
-                                        .json(InternalServerErrorJson {
-                                            reason,
-                                            is_bug: true,
-                                            link: Some(
-                                                "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
-                                                labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+fire+event"
-                                                    .to_string(),
-                                            ),
-                                        })
-                                } else {
-                                    HttpResponse::Gone().finish()
-                                }
-                            }
-                            Err(e) => {
-                                HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                                    reason: format!("DB returned an error: {}", e),
-                                    is_bug: false,
-                                    link: None,
-                                })
-                            }
-                        };
-                    }
-                }
-
-                let already_exists = sqlx::query!(
-                    r#"SELECT EXISTS(SELECT * FROM members WHERE user_id = $1 AND guild_id = $2) AS "exists!""#,
-                    bigint_user_id,
-                    invite.guild_id
-                )
-                .fetch_one(db)
-                .await;
-                match already_exists {
-                    Ok(r) => {
-                        if r.exists {
-                            return HttpResponse::Conflict().json(Json {
-                                message: "user has already joined this guild".to_string(),
-                            });
-                        }
-                    }
-                    Err(e) => {
-                        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                            reason: format!("DB returned an error: {}", e),
-                            is_bug: false,
-                            link: None,
-                        })
-                    }
-                }
-
-                let member_resp = sqlx::query!(
-                    "INSERT INTO members VALUES ($1, $2)",
-                    bigint_user_id,
-                    invite.guild_id
-                )
-                .execute(db)
-                .await;
-
-                let member_obj = match member_resp {
-                    Ok(_) => Member {
-                        user_id: Some(user_id),
-                        user: match sqlx::query!(
-                            "SELECT * FROM users WHERE id = $1",
-                            bigint_user_id
-                        )
-                        .fetch_optional(db)
-                        .await
-                        {
-                            Ok(o) => match o {
-                                Some(u) => Some(User {
-                                    id: user_id,
-                                    name: u.name.clone(),
-                                    avatar: None,
-                                    guilds: None,
-                                    flags: UserFlags::from_bits_truncate(u.flags),
-                                    discriminator: u.discriminator,
-                                    pronouns: u
-                                        .pronouns
-                                        .and_then(ferrischat_common::types::Pronouns::from_i16),
-                                }),
-                                None => None,
-                            },
-                            Err(e) => {
-                                return HttpResponse::InternalServerError().json(
-                                    InternalServerErrorJson {
-                                        reason: format!("DB returned an error: {}", e),
-                                        is_bug: false,
-                                        link: None,
-                                    },
-                                )
-                            }
-                        },
-                        guild_id: Some(guild_id),
-                        guild: None,
-                    },
-                    Err(e) => {
-                        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                            reason: format!("DB returned an error: {}", e),
-                            is_bug: false,
-                            link: None,
-                        })
-                    }
-                };
-
-                let uses_resp = sqlx::query!(
-                    "UPDATE invites SET uses = $1 WHERE code = $2",
-                    uses,
-                    invite_code
-                )
-                .execute(db)
-                .await;
-
-                match uses_resp {
-                    Ok(_) => (),
-                    Err(e) => {
-                        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                            reason: format!("DB returned an error: {}", e),
-                            is_bug: false,
-                            link: None,
-                        })
-                    }
-                }
-
-                member_obj
-            }
-            None => return HttpResponse::NotFound().finish(),
-        },
-        Err(e) => {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            })
+    }
+    if let Some(max_age) = invite.max_age {
+        if (now - invite.created_at) > max_age {
+            to_delete = true;
         }
-    };
+    }
+    if to_delete {
+        sqlx::query!("DELETE FROM invites WHERE code = $1", invite_code)
+            .execute(db)
+            .await?;
 
-    let event = WsOutboundEvent::MemberCreate { member: member_obj };
-
-    if let Err(e) = fire_event(format!("member_{}", guild_id), &event).await {
-        let reason = match e {
-            WsEventError::MissingRedis => "Redis pool missing".to_string(),
-            WsEventError::RedisError(e) => format!("Redis returned an error: {}", e),
-            WsEventError::JsonError(e) => {
-                format!("Failed to serialize message to JSON format: {}", e)
-            }
-            WsEventError::PoolError(e) => format!("`deadpool` returned an error: {}", e),
+        let invite_obj = Invite {
+            code: invite.code.clone(),
+            owner_id: bigdecimal_to_u128!(invite.owner_id),
+            guild_id,
+            created_at: invite.created_at,
+            uses,
+            max_uses: invite.max_uses,
+            max_age: invite.max_age,
         };
-        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-            reason,
-            is_bug: true,
-            link: Some(
-                "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
-                labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+fire+event"
-                    .to_string(),
-            ),
-        });
+
+        let event = WsOutboundEvent::InviteDelete { invite: invite_obj };
+
+        fire_event(format!("invite_{}", guild_id), &event).await?;
+
+        return Err((
+            410,
+            ferrischat_common::types::Json {
+                message: "this invite just disappeared".to_string(),
+            },
+        )
+            .into());
     }
 
-    HttpResponse::Created().finish()
+    if sqlx::query!(
+        r#"SELECT EXISTS(SELECT * FROM members WHERE user_id = $1 AND guild_id = $2) AS "exists!""#,
+        bigint_user_id,
+        bigint_guild_id
+    )
+    .fetch_one(db)
+    .await?
+    .exists
+    {
+        return Err((
+            409,
+            ferrischat_common::types::Json {
+                message: "user has already joined this guild".to_string(),
+            },
+        )
+            .into());
+    };
+
+    let member_resp = sqlx::query!(
+        "INSERT INTO members VALUES ($1, $2)",
+        bigint_user_id,
+        invite.guild_id
+    )
+    .execute(db)
+    .await?;
+
+    let member_obj = Member {
+        user_id: Some(user_id),
+        user: Some({
+            let u = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_user_id)
+                .fetch_one(db)
+                .await?;
+            User {
+                id: user_id,
+                name: u.name.clone(),
+                avatar: None,
+                guilds: None,
+                flags: UserFlags::from_bits_truncate(u.flags),
+                discriminator: u.discriminator,
+                pronouns: u
+                    .pronouns
+                    .and_then(ferrischat_common::types::Pronouns::from_i16),
+            }
+        }),
+        guild_id: Some(guild_id),
+        guild: None,
+    };
+
+    sqlx::query!(
+        "UPDATE invites SET uses = $1 WHERE code = $2",
+        uses,
+        invite_code
+    )
+    .execute(db)
+    .await?;
+
+    let event = WsOutboundEvent::MemberCreate {
+        member: member_obj.clone(),
+    };
+
+    fire_event(format!("member_{}", guild_id), &event).await?;
+    Ok(crate::Json {
+        obj: member_obj,
+        code: 201,
+    })
 }
