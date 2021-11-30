6f2f7b020ddb3003aef07391c4c45cfdb291aa58
0/0 <zero@imaskeleton.me>
2021-11-30 08:26:37 -0700

    fix create_invite.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/invites/create_invite.rs
+++ ferrischat_webserver/src/invites/create_invite.rs
@@ -1,53 +1,47 @@
-use crate::ws::{fire_event, WsEventError};
-use actix_web::web::Json;
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use crate::ws::fire_event;
+use crate::WebServerError;
+use axum::extract::Path;
+use axum::Json;
 use ferrischat_common::request_json::InviteCreateJson;
-use ferrischat_common::types::{InternalServerErrorJson, Invite};
+use ferrischat_common::types::Invite;
 use ferrischat_common::ws::WsOutboundEvent;
 use ferrischat_snowflake_generator::FERRIS_EPOCH;
+use serde::Serialize;
 use sqlx::types::time::OffsetDateTime;
 
 /// POST `/api/v0/guilds/{guild_id}/invites`
 pub async fn create_invite(
     auth: crate::Authorization,
-    invite_info: Json<InviteCreateJson>,
-    req: HttpRequest,
-) -> impl Responder {
+    Path(guild_id): Path<u128>,
+    Json(InviteCreateJson { max_age, max_uses }): Json<InviteCreateJson>,
+) -> Result<crate::Json<Invite>, WebServerError<impl Serialize>> {
     let db = get_db_or_fail!();
-    let InviteCreateJson { max_age, max_uses } = invite_info.0;
 
-    let guild_id = get_item_id!(req, "guild_id");
     let bigint_guild_id = u128_to_bigdecimal!(guild_id);
 
     let owner_id = auth.0;
     let bigint_owner_id = u128_to_bigdecimal!(owner_id);
 
+    if sqlx::query!(
+        "SELECT user_id FROM members WHERE user_id = $1 AND guild_id = $2",
+        bigint_owner_id,
+        bigint_guild_id
+    )
+    .fetch_optional(db)
+    .await?
+    .is_none()
+    {
+        return Err((
+            403,
+            ferrischat_common::types::Json {
+                message: "you are not a member of this guild".to_string(),
+            },
+        )
+            .into());
+    }
+
     let now = OffsetDateTime::now_utc().unix_timestamp()
         - (i64::try_from(FERRIS_EPOCH).expect("failed to cast the Ferris Epoch to i64"));
-    {
-        let resp = sqlx::query!(
-            "SELECT user_id FROM members WHERE user_id = $1 AND guild_id = $2",
-            bigint_owner_id,
-            bigint_guild_id
-        )
-        .fetch_optional(db)
-        .await;
-
-        match resp {
-            Ok(resp) => {
-                if resp.is_none() {
-                    return HttpResponse::Forbidden().finish();
-                }
-            }
-            Err(e) => {
-                return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                    reason: format!("DB returned an error: {}", e),
-                    is_bug: false,
-                    link: None,
-                });
-            }
-        }
-    }
     let resp = sqlx::query!(
         "INSERT INTO invites VALUES ((SELECT array_to_string( \
             ARRAY(SELECT substr( \
@@ -62,50 +56,25 @@ pub async fn create_invite(
         max_age
     )
     .fetch_one(db)
-    .await;
+    .await?;
 
-    let invite_obj = match resp {
-        Ok(code) => Invite {
-            code: code.code,
-            owner_id,
-            guild_id,
-            created_at: now,
-            uses: 0,
-            max_uses,
-            max_age,
-        },
-        Err(e) => {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            })
-        }
+    let invite_obj = Invite {
+        code: resp.code,
+        owner_id,
+        guild_id,
+        created_at: now,
+        uses: 0,
+        max_uses,
+        max_age,
     };
 
     let event = WsOutboundEvent::InviteCreate {
         invite: invite_obj.clone(),
     };
 
-    if let Err(e) = fire_event(format!("invite_{}", guild_id), &event).await {
-        let reason = match e {
-            WsEventError::MissingRedis => "Redis pool missing".to_string(),
-            WsEventError::RedisError(e) => format!("Redis returned an error: {}", e),
-            WsEventError::JsonError(e) => {
-                format!("Failed to serialize message to JSON format: {}", e)
-            }
-            WsEventError::PoolError(e) => format!("`deadpool` returned an error: {}", e),
-        };
-        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-            reason,
-            is_bug: true,
-            link: Some(
-                "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
-                labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+fire+event"
-                    .to_string(),
-            ),
-        });
-    }
-
-    HttpResponse::Created().json(invite_obj)
+    fire_event(format!("invite_{}", guild_id), &event).await?;
+    Ok(crate::Json {
+        obj: invite_obj,
+        code: 201,
+    })
 }
