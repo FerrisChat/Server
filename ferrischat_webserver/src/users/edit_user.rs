bf45cac7d10fe963666236c7dd9bf2037001e2b8
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-11-29 21:49:30 -0500

    Rewrite edit_user

--- ferrischat_webserver/src/users/edit_user.rs
+++ ferrischat_webserver/src/users/edit_user.rs
@@ -1,164 +1,135 @@
-use actix_web::web::Json;
+use crate::WebServerError;
+use axum::extract::{Json, Path};
 
-use actix_web::{HttpRequest, HttpResponse, Responder};
 use ferrischat_common::request_json::UserUpdateJson;
 use ferrischat_common::types::{InternalServerErrorJson, NotFoundJson, User, UserFlags};
 
 use tokio::sync::oneshot::channel;
+use serde::Serialize;
 
+/// PATCH `/api/v0/users/{user_id}`
+/// Modifies the authenticated user
 pub async fn edit_user(
-    req: HttpRequest,
-    user_info: Json<UserUpdateJson>,
+    Path(user_id): Path<u128>,
+    Json(UserUpdateJson { username, email, password, pronouns, .. }): Json<UserUpdateJson>,
     auth: crate::Authorization,
-) -> impl Responder {
-    let user_id = get_item_id!(req, "user_id");
-
+) -> Result<crate::Json<User>, WebServerError<impl Serialize>> {
     if user_id != auth.0 {
-        return HttpResponse::Forbidden().finish();
+        return Err((
+            403,
+            Json {
+                message: "this account is not yours".to_string(),
+            },
+        )
+            .into());
     }
 
     let bigint_user_id = u128_to_bigdecimal!(user_id);
-
-    let UserUpdateJson {
-        username,
-        email,
-        password,
-        avatar: _,
-        pronouns,
-    } = user_info.0;
-
     let db = get_db_or_fail!();
 
     if let Some(username) = username {
-        let resp = sqlx::query!(
+        sqlx::query!(
             "UPDATE users SET name = $1 WHERE id = $2",
             username,
-            bigint_user_id
+            bigint_user_id,
         )
         .execute(db)
-        .await;
-        if let Err(e) = resp {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            });
-        }
+        .await?;
     }
 
     if let Some(email) = email {
-        let resp = sqlx::query!(
+        sqlx::query!(
             "UPDATE users SET email = $1 WHERE id = $2",
             email,
-            bigint_user_id
+            bigint_user_id,
         )
         .execute(db)
-        .await;
-        if let Err(e) = resp {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            });
-        }
+        .await?;
     }
 
     if let Some(password) = password {
         let hashed_password = {
-            let hasher = match ferrischat_auth::GLOBAL_HASHER.get() {
-                Some(h) => h,
-                None => {
-                    return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                        reason: "Password hasher not found".to_string(),
-                        is_bug: false,
-                        link: None,
-                    })
-                }
-            };
-
             let (tx, rx) = channel();
-
-            let _tx = hasher.send((password, tx)).await;
-            match rx.await {
-                Ok(d) => match d {
-                    Ok(s) => s,
-                    Err(e) => {
-                        return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                            reason: format!("Failed to hash password: {}", e),
+            ferrischat_auth::GLOBAL_HASHER
+                .get()
+                .ok_or(WebServerError::MissingHasher)?
+                .send((password, tx))
+                .await
+                .map_err(|_| {
+                    (
+                        500,
+                        InternalServerErrorJson {
+                            reason: "Password hasher has hung up connection".to_string(),
                             is_bug: false,
                             link: None,
-                        })
-                    }
-                },
-                Err(_) => {
-                    return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                        reason: "Other end hung up connection".to_string(),
-                        is_bug: false,
-                        link: None,
-                    })
-                }
-            }
+                        },
+                    )
+                        .into()
+                })?;
+            rx.await
+            .unwrap_or_else(|e| {
+                unreachable!(
+                    "failed to receive value from channel despite value being sent earlier on: {}",
+                    e
+                )
+            })
+            .map_err(|e| {
+                (
+                    500,
+                    InternalServerErrorJson {
+                        reason: format!("failed to hash token: {}", e),
+                        is_bug: true,
+                        link: Some(
+                            "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
+                             labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+hash+token"
+                                .to_string(),
+                        ),
+                    },
+                )
+                    .into()
+            })?
         };
-        let resp = sqlx::query!(
+        sqlx::query!(
             "UPDATE users SET password = $1 WHERE id = $2",
             hashed_password,
             bigint_user_id
         )
         .execute(db)
-        .await;
-        if let Err(e) = resp {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            });
-        }
+        .await?;
     }
 
     if let Some(pronouns) = pronouns {
-        let resp = sqlx::query!(
+        sqlx::query!(
             "UPDATE users SET pronouns = $1 WHERE id = $2",
             pronouns as i16,
             bigint_user_id
         )
         .execute(db)
-        .await;
-        if let Err(e) = resp {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            });
-        }
+        .await?;
     }
 
-    let resp = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_user_id)
+    let user = sqlx::query!("SELECT * FROM users WHERE id = $1", bigint_user_id)
         .fetch_optional(db)
-        .await;
+        .await?
+        .ok_or_else(|| (
+            404,
+            NotFoundJson {
+                message: format!("unknown user with id {}", user_id),
+            },
+        ))?;
 
-    match resp {
-        Ok(resp) => match resp {
-            Some(user) => HttpResponse::Ok().json(User {
-                id: user_id,
-                name: user.name.clone(),
-                avatar: None,
-                guilds: None,
-                flags: UserFlags::from_bits_truncate(user.flags),
-                discriminator: user.discriminator,
-                pronouns: user
-                    .pronouns
-                    .and_then(ferrischat_common::types::Pronouns::from_i16),
-            }),
-            None => HttpResponse::NotFound().json(NotFoundJson {
-                message: format!("Unknown user with id {}", user_id),
-            }),
+    Ok(crate::Json {
+        code: 200,
+        obj: User {
+            id: user_id,
+            name: user.name.clone(),
+            avatar: None,
+            guilds: None,
+            flags: UserFlags::from_bits_truncate(user.flags),
+            discriminator: user.discriminator,
+            pronouns: user
+                .pronouns
+                .and_then(ferrischat_common::types::Pronouns::from_i16),
         },
-        Err(e) => {
-            return HttpResponse::InternalServerError().json(InternalServerErrorJson {
-                reason: format!("DB returned an error: {}", e),
-                is_bug: false,
-                link: None,
-            })
-        }
-    }
+    })
 }
