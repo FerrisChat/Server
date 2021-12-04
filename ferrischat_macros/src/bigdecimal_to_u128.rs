8f3a5c401ff52610972990aae5835ef7a069a2f8
0/0 <zero@imaskeleton.me>
2021-12-03 22:22:51 -0700

    fix ~50 errors
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_macros/src/bigdecimal_to_u128.rs
+++ ferrischat_macros/src/bigdecimal_to_u128.rs
@@ -3,18 +3,15 @@
 /// or returns a HTTP 500 from the function.
 macro_rules! bigdecimal_to_u128 {
     ($decimal:expr) => {{
-        use ferrischat_common::types::InternalServerErrorJson;
+        use ferrischat_common::types::ErrorJson;
         use num_traits::cast::ToPrimitive;
         $decimal
             .with_scale(0)
             .into_bigint_and_exponent()
             .0
-            .to_u128().ok_or(Err((500, InternalServerErrorJson {
-            reason: "snowflake ID overflowed 128 bit integer".to_string(),
-            is_bug: true,
-            link: Some(
-                "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+snowflake+overflowed+u128"
-                    .to_string()),
-        }).into()))?
+            .to_u128().ok_or_else(|| ErrorJson::new_500("snowflake ID overflowed 128 bit integer".to_string(), true, Some(
+            "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+snowflake+overflowed+u128"
+                .to_string())))
+            ?
     }};
 }
