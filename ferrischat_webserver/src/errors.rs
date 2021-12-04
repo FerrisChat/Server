2fafae060ab64adca01da4f17c5cefacf866c143
0/0 <zero@imaskeleton.me>
2021-12-03 21:33:31 -0700

    switch webserver error enum to use the single unified type
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/errors.rs
+++ ferrischat_webserver/src/errors.rs
@@ -1,176 +1,116 @@
 use axum::http::Response;
 use axum::response::IntoResponse;
-use ferrischat_common::types::InternalServerErrorJson;
+use ferrischat_common::types::{ErrorJson, InternalServerErrorJson};
 use ferrischat_redis::deadpool_redis::redis::RedisError;
 use ferrischat_redis::deadpool_redis::PoolError;
 use serde::Serialize;
 use sqlx::Error;
 
-pub enum WebServerError<T: Serialize> {
+pub enum WebServerError {
     Database(sqlx::Error),
     MissingDatabase,
     Json(simd_json::Error),
     Redis(RedisError),
     MissingRedis,
     RedisPool(PoolError),
-    Http { code: u16, body: T },
+    Http(ErrorJson),
     RandomGenerationFailure,
     MissingHasher,
     MissingVerifier,
     MissingNodeId,
 }
 
-impl<T: Serialize> From<PoolError> for WebServerError<T> {
+impl From<PoolError> for WebServerError {
     fn from(e: PoolError) -> Self {
         Self::RedisPool(e)
     }
 }
 
-impl<T: Serialize> From<sqlx::Error> for WebServerError<T> {
+impl From<sqlx::Error> for WebServerError {
     fn from(e: Error) -> Self {
         Self::Database(e)
     }
 }
 
-impl<T: Serialize> From<simd_json::Error> for WebServerError<T> {
+impl From<simd_json::Error> for WebServerError {
     fn from(e: simd_json::Error) -> Self {
         Self::Json(e)
     }
 }
 
-impl<T: Serialize> From<RedisError> for WebServerError<T> {
+impl From<RedisError> for WebServerError {
     fn from(e: RedisError) -> Self {
         Self::Redis(e)
     }
 }
 
-impl<T: Serialize> From<(u16, T)> for WebServerError<T> {
-    fn from((code, obj): (u16, T)) -> Self {
-        if !(100 <= code && code <= 599) {
-            panic!("invalid HTTP code passed to .from() for WebServerError (valid codes are 100 <= code <= 599)");
-        }
-        Self { code, obj }
+impl From<ErrorJson> for WebServerError {
+    fn from(e: ErrorJson) -> Self {
+        Self::Http(e)
     }
 }
 
-impl<T: Serialize> IntoResponse for WebServerError<T> {
+impl IntoResponse for WebServerError {
     type Body = axum::body::Body;
     type BodyError = <Self::Body as axum::body::HttpBody>::Error;
 
     fn into_response(self) -> Response<Self::Body> {
-        let (code, body) = match self {
+        let body = match self {
             WebServerError::Database(e) => {
                 if let sqlx::Error::Database(e) = e {
                     if e.code() == "23505".into() {
-                        (
-                            409,
-                            ferrischat_common::types::Json {
-                                message: "This object is a duplicate.".to_string()
-                            },
-                        )
+                        ErrorJson::new_409("This object is a duplicate.".to_string())
                     } else {
-                        (
-                            500,
-                            InternalServerErrorJson {
-                                reason: format!("Database returned an error: {:?}", e),
-                                is_bug: false,
-                                link: None,
-                            },
-                        )
+                        ErrorJson::new_500(format!("Database returned an error: {:?}", e), false, None)
                     }
                 } else {
-                    (
-                        500,
-                        InternalServerErrorJson {
-                            reason: format!("Database returned an error: {:?}", e),
-                            is_bug: false,
-                            link: None,
-                        },
+                    ErrorJson::new_500(
+                        format!("Database returned an error: {:?}", e),
+                        false,
+                        None,
                     )
                 }
             }
-            WebServerError::MissingDatabase => (
-                500,
-                InternalServerErrorJson {
-                    reason: "Database pool was not found".to_string(),
-                    is_bug: false,
-                    link: None,
-                },
+            WebServerError::MissingDatabase => ErrorJson::new_500(
+                "Database pool was not found".to_string(),
+                false,
+                None,
             ),
-            WebServerError::Json(e) => (
-                500,
-                InternalServerErrorJson {
-                    reason: format!("JSON (de)serialization failed: {}", e),
-                    is_bug: false,
-                    link: None,
-                },
-            ),
-            WebServerError::Http { code, body } => (code, body),
-            WebServerError::Redis(e) => (
-                500,
-                InternalServerErrorJson {
-                    reason: format!("Redis returned an error: {}", e),
-                    is_bug: false,
-                    link: None,
-                },
-            ),
-            WebServerError::MissingRedis => (
-                500,
-                InternalServerErrorJson {
-                    reason: "Redis pool missing".to_string(),
-                    is_bug: false,
-                    link: None,
-                },
-            ),
-            WebServerError::RedisPool(e) => (
-                500,
-                InternalServerErrorJson {
-                    reason: format!("Redis pool returned an error: {}", e),
-                    is_bug: false,
-                    link: None,
-                },
-            ),
-            WebServerError::RandomGenerationFailure => (
-                500,
-                InternalServerErrorJson {
-                    reason: "failed to generate random bits for token generation".to_string(),
-                    is_bug: true,
-                    link: Some(
-                        "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
+            WebServerError::Json(e) => ErrorJson::new_500(format!("JSON (de)serialization failed: {}", e), false, None),
+            WebServerError::Http(e) => e,
+            WebServerError::Redis(e) => ErrorJson::new_500(format!("Redis returned an error: {}", e), false, None),
+            WebServerError::MissingRedis => ErrorJson::new_500("Redis pool missing".to_string(), false, None),
+            WebServerError::RedisPool(e) => ErrorJson::new_500(format!("Redis pool returned an error: {}", e), false, None),
+            WebServerError::RandomGenerationFailure => ErrorJson::new_500(
+                "failed to generate random bits for token generation".to_string(),
+                true,
+                Some(
+                    "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
                     labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+failed+to+generate+random+bits"
-                            .to_string(),
-                    ),
-                }),
-            WebServerError::MissingHasher => (
-                500,
-                InternalServerErrorJson {
-                    reason: "password hasher not found".to_string(),
-                    is_bug: false,
-                    link: None,
-                },
+                        .to_string(),
+                ),
             ),
-            WebServerError::MissingVerifier => (
-                500,
-                InternalServerErrorJson {
-                    reason: "password verifier not found".to_string(),
-                    is_bug: true,
-                    link: Some(
-                        "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
+            WebServerError::MissingHasher => ErrorJson::new_500(
+                "password hasher not found".to_string(),
+                false,
+                None,
+            ),
+            WebServerError::MissingVerifier => ErrorJson::new_500(
+                "password verifier not found".to_string(),
+                true,
+                Some(
+                    "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&\
                         labels=bug&template=api_bug_report.yml&title=%5B500%5D%3A+password+verifier+not+found"
-                            .to_string(),
-                    ),
-                },
-            ),
-            WebServerError::MissingNodeId => (
-                500,
-                InternalServerErrorJson {
-                    reason: "Redis has not been set up yet".to_string(),
-                    is_bug: true,
-                    link: Some(
-                        "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&labels=bug&\
+                        .to_string(),
+                )),
+
+            WebServerError::MissingNodeId => ErrorJson::new_500(
+                "Redis has not been set up yet".to_string(),
+                true,
+                Some(
+                    "https://github.com/FerrisChat/Server/issues/new?assignees=tazz4843&labels=bug&\
                     template=api_bug_report.yml&title=%5B500%5D%3A+redis+not+set+up"
-                            .to_string()),
-                }
+                        .to_string()),
             ),
         };
 
@@ -186,12 +126,12 @@ impl<T: Serialize> IntoResponse for WebServerError<T> {
         };
 
         axum::http::Response::builder()
-            .status(code)
+            .status(body.get_code())
             .body(axum::body::Body::from(bytes))
             .unwrap_or_else(|e| {
                 // this should only be reachable if a invalid HTTP code is passed in
                 unreachable!(
-                    "got an error while attempting to construct HTTP response for 500: {}",
+                    "got an error while attempting to construct HTTP response for ServerError: {}",
                     e
                 )
             })
