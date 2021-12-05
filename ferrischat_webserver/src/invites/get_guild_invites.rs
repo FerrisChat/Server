673a306a23e88cd510fa1f4a63d1353ebd5e849b
0/0 <zero@imaskeleton.me>
2021-12-04 19:06:54 -0700

    fix get_guild_invites.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/invites/get_guild_invites.rs
+++ ferrischat_webserver/src/invites/get_guild_invites.rs
@@ -1,7 +1,6 @@
 use crate::WebServerError;
 use axum::extract::Path;
-use ferrischat_common::types::{Invite, ErrorJson};
-use serde::Serialize;
+use ferrischat_common::types::{ErrorJson, Invite};
 
 /// GET `/api/v0/guilds/{guild_id}/invites`
 pub async fn get_guild_invites(
@@ -21,30 +20,24 @@ pub async fn get_guild_invites(
     .await?
     .is_none()
     {
-        return Err(ErrorJson::new_403(
-            "you are not a member of this guild".to_string(),
-        ).into());
+        return Err(ErrorJson::new_403("you are not a member of this guild".to_string()).into());
     }
 
     let res_invites = sqlx::query!("SELECT * FROM invites WHERE guild_id = $1", bigint_guild_id)
         .fetch_all(db)
-        .await?
-        .iter()
-        .map(|invite| {
-            Ok(Invite {
-                code: invite.code,
-                owner_id: bigdecimal_to_u128!(invite.owner_id),
-                guild_id: bigdecimal_to_u128!(invite.guild_id),
-                created_at: invite.created_at,
-                uses: invite.uses,
-                max_uses: invite.max_uses,
-                max_age: invite.max_age,
-            })
-        });
+        .await?;
 
     let mut invites = Vec::with_capacity(res_invites.len());
     for invite in res_invites {
-        invites.push(invite?);
+        invites.push(Invite {
+            code: invite.code,
+            owner_id: bigdecimal_to_u128!(invite.owner_id),
+            guild_id: bigdecimal_to_u128!(invite.guild_id),
+            created_at: invite.created_at,
+            uses: invite.uses,
+            max_uses: invite.max_uses,
+            max_age: invite.max_age,
+        });
     }
 
     Ok(crate::Json {
