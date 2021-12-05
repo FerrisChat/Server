aac008467c78af0880b383e584070ac56f7e0655
0/0 <zero@imaskeleton.me>
2021-12-04 19:56:41 -0700

    fix get_role.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/guilds/roles/get_role.rs
+++ ferrischat_webserver/src/guilds/roles/get_role.rs
@@ -2,7 +2,6 @@ use crate::WebServerError;
 use axum::extract::Path;
 use ferrischat_common::perms::Permissions;
 use ferrischat_common::types::{ErrorJson, Role};
-use serde::Serialize;
 
 /// GET `/api/v0/guilds/{guild_id/roles/{role_id}`
 pub async fn get_role(
@@ -10,27 +9,19 @@ pub async fn get_role(
     _: crate::Authorization,
 ) -> Result<crate::Json<Role>, WebServerError> {
     let bigint_role_id = u128_to_bigdecimal!(role_id);
-    Ok(
-        sqlx::query!("SELECT * FROM roles where id = $1", bigint_role_id)
-            .fetch_optional(get_db_or_fail!())
-            .await?
-            .map(|r| crate::Json {
-                obj: Role {
-                    id: role_id,
-                    name: r.name,
-                    color: r.color,
-                    position: r.position,
-                    guild_id: bigdecimal_to_u128!(r.parent_guild),
-                    permissions: Permissions::from_bits_truncate(r.permissions),
-                },
-                code: 200,
-            })
-            .ok_or_else(|| {
-                (
-                    404,
-                    ErrorJson::new_404(format!("Unknown role with ID {}", role_id)),
-                )
-                    .into()
-            })?,
-    )
+    let r = sqlx::query!("SELECT * FROM roles where id = $1", bigint_role_id)
+        .fetch_optional(get_db_or_fail!())
+        .await?
+        .ok_or_else(|| ErrorJson::new_404(format!("Unknown role with ID {}", role_id)))?;
+    Ok(crate::Json {
+        obj: Role {
+            id: role_id,
+            name: r.name,
+            color: r.color,
+            position: r.position,
+            guild_id: bigdecimal_to_u128!(r.parent_guild),
+            permissions: Permissions::from_bits_truncate(r.permissions),
+        },
+        code: 200,
+    })
 }
