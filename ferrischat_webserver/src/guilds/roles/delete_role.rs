e3b82df05d237f21924b0da69e9597c96390bdf4
0/0 <zero@imaskeleton.me>
2021-12-04 19:59:23 -0700

    fix delete_role.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/guilds/roles/delete_role.rs
+++ ferrischat_webserver/src/guilds/roles/delete_role.rs
@@ -5,33 +5,32 @@ use ferrischat_common::perms::Permissions;
 use ferrischat_common::types::{ErrorJson, Role};
 use ferrischat_common::ws::WsOutboundEvent;
 use http::StatusCode;
-use serde::Serialize;
 
 /// DELETE `/api/v0/guilds/{guild_id/roles/{role_id}`
 pub async fn delete_role(
     Path((guild_id, role_id)): Path<(u128, u128)>,
     _: crate::Authorization,
-) -> Result<StatusCode, WebServerError<impl Serialize>> {
+) -> Result<StatusCode, WebServerError> {
     let db = get_db_or_fail!();
     let bigint_role_id = u128_to_bigdecimal!(role_id);
     let bigint_guild_id = u128_to_bigdecimal!(guild_id);
 
-    let role_obj: Role = sqlx::query!(
+    let role = sqlx::query!(
         "DELETE FROM roles WHERE id = $1 AND parent_guild = $2 RETURNING *",
         bigint_role_id,
         bigint_guild_id
     )
     .fetch_optional(db)
     .await?
-    .map(|role| Role {
+    .ok_or_else(|| ErrorJson::new_404(format!("Unknown role with ID {}", role_id)))?;
+    let role_obj = Role {
         id: bigdecimal_to_u128!(role.id),
         guild_id: bigdecimal_to_u128!(role.parent_guild),
         name: role.name,
         color: role.color,
         position: role.position,
         permissions: Permissions::from_bits_truncate(role.permissions),
-    })
-    .ok_or_else(|| ErrorJson::new_404(format!("Unknown role with ID {}", role_id)))?;
+    };
 
     let event = WsOutboundEvent::RoleDelete {
         role: role_obj.clone(),
