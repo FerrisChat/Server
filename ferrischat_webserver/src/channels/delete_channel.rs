b7694df2e0c03839e5e85bc5adbaa70c37ba6048
0/0 <zero@imaskeleton.me>
2021-12-04 18:44:56 -0700

    fix delete_channel.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/channels/delete_channel.rs
+++ ferrischat_webserver/src/channels/delete_channel.rs
@@ -3,7 +3,6 @@ use crate::WebServerError;
 use axum::extract::Path;
 use ferrischat_common::types::{Channel, ErrorJson};
 use ferrischat_common::ws::WsOutboundEvent;
-use serde::Serialize;
 
 /// DELETE `/api/v0/channels/{channel_id}`
 pub async fn delete_channel(
@@ -13,32 +12,23 @@ pub async fn delete_channel(
     let db = get_db_or_fail!();
     let bigint_channel_id = u128_to_bigdecimal!(channel_id);
 
-    let channel_obj = sqlx::query!(
+    let channel = sqlx::query!(
         "DELETE FROM channels WHERE id = $1 RETURNING *",
         bigint_channel_id,
     )
-        .fetch_optional(db)
-        .await?
-        .map(|channel| Channel {
-            id: bigdecimal_to_u128!(channel.id),
-            guild_id: bigdecimal_to_u128!(channel.guild_id),
-            name: channel.name,
-        })
-        .ok_or_else(||
-                        ErrorJson::new_404(
-                            format!("Unknown channel with ID {}", channel_id),
-                        ),
-        )?;
-
-    let event = WsOutboundEvent::ChannelDelete {
-        channel: channel_obj.clone(),
+    .fetch_optional(db)
+    .await?
+    .ok_or_else(|| ErrorJson::new_404(format!("Unknown channel with ID {}", channel_id)))?;
+    let channel = Channel {
+        id: bigdecimal_to_u128!(channel.id),
+        guild_id: bigdecimal_to_u128!(channel.guild_id),
+        name: channel.name,
     };
+    let guild_id = channel.guild_id;
 
-    fire_event(
-        format!("channel_{}_{}", channel_id, channel_obj.guild_id),
-        &event,
-    )
-    .await?;
+    let event = WsOutboundEvent::ChannelDelete { channel };
+
+    fire_event(format!("channel_{}_{}", channel_id, guild_id), &event).await?;
 
     Ok(http::StatusCode::NO_CONTENT)
 }
