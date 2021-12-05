7900919a5017c55a8cdaa71111f7e7b9b396b3e6
0/0 <zero@imaskeleton.me>
2021-12-04 19:26:10 -0700

    fix get_message.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/messages/get_messages.rs
+++ ferrischat_webserver/src/messages/get_messages.rs
@@ -1,7 +1,6 @@
 use crate::WebServerError;
 use axum::extract::Path;
-use ferrischat_common::types::{Message, ErrorJson, User, UserFlags};
-use serde::Serialize;
+use ferrischat_common::types::{ErrorJson, Message, User, UserFlags};
 
 /// GET `/api/v0/guilds/{guild_id}/channels/{channel_id}/messages/{message_id}`
 pub async fn get_message(
@@ -10,7 +9,7 @@ pub async fn get_message(
 ) -> Result<crate::Json<Message>, WebServerError> {
     let bigint_message_id = u128_to_bigdecimal!(message_id);
     let bigint_channel_id = u128_to_bigdecimal!(channel_id);
-    Ok(
+    let m =
         sqlx::query!(
             "SELECT m.*, a.pronouns AS author_pronouns, a.avatar AS author_avatar, a.name AS author_name, a.flags AS author_flags, a.discriminator AS author_discriminator FROM messages m CROSS JOIN LATERAL (SELECT * FROM users WHERE id = m.author_id) AS a WHERE m.id = $1 AND m.channel_id = $2",
             bigint_message_id,
@@ -18,36 +17,32 @@ pub async fn get_message(
         )
             .fetch_optional(get_db_or_fail!())
             .await?
-            .map(|m| crate::Json {
-                obj: Message {
-                    id: message_id,
-                    content: m.conent,
-                    channel_id,
-                    author_id: bigdecimal_to_u128!(m.author_id),
-                    edited_at: m.edited_at,
-                    embeds: vec![],
-                    author: Some(User {
-                        id: bigdecimal_to_u128!(m.author_id),
-                        name: m.author_name,
-                        avatar: m.author_avatar,
-                        guilds: None,
-                        flags: UserFlags::from_bits_truncate(m.author_flags),
-                        discriminator: m.author_discriminator,
-                        pronouns: m.author_pronouns
-                            .and_then(ferrischat_common::types::Pronouns::from_i16),
-                    }),
-                    nonce: None,
-                },
-                code: 200,
-            })
             .ok_or_else(|| {
-                (
-                    404,
-                    ErrorJson::new_404(
-                        format!("Unknown message with ID {}", message_id),
-                    ),
+                ErrorJson::new_404(
+                    format!("Unknown message with ID {}", message_id),
                 )
-                    .into()
-            })?
-    )
+            })?;
+    Ok(crate::Json {
+        obj: Message {
+            id: message_id,
+            content: m.content,
+            channel_id,
+            author_id: bigdecimal_to_u128!(m.author_id),
+            edited_at: m.edited_at,
+            embeds: vec![],
+            author: Some(User {
+                id: bigdecimal_to_u128!(m.author_id),
+                name: m.author_name,
+                avatar: m.author_avatar,
+                guilds: None,
+                flags: UserFlags::from_bits_truncate(m.author_flags),
+                discriminator: m.author_discriminator,
+                pronouns: m
+                    .author_pronouns
+                    .and_then(ferrischat_common::types::Pronouns::from_i16),
+            }),
+            nonce: None,
+        },
+        code: 200,
+    })
 }
