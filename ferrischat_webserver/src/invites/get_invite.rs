f609810f0cd764265cba71a58a8b858bc6265f7e
0/0 <zero@imaskeleton.me>
2021-12-04 19:08:24 -0700

    fix get_invite.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/invites/get_invite.rs
+++ ferrischat_webserver/src/invites/get_invite.rs
@@ -1,35 +1,26 @@
 use crate::WebServerError;
 use axum::extract::Path;
-use ferrischat_common::types::{Invite, ErrorJson};
-use serde::Serialize;
+use ferrischat_common::types::{ErrorJson, Invite};
 
 /// GET api/v0/invites/{code}
 pub async fn get_invite(
     Path(code): Path<String>,
     _: crate::Authorization,
 ) -> Result<crate::Json<Invite>, WebServerError> {
-    Ok(sqlx::query!("SELECT * FROM invites WHERE code = $1", code)
+    let r = sqlx::query!("SELECT * FROM invites WHERE code = $1", code)
         .fetch_optional(get_db_or_fail!())
         .await?
-        .map(|r| crate::Json {
-            obj: Invite {
-                code: code.to_string(),
-                owner_id: bigdecimal_to_u128!(r.owner_id),
-                guild_id: bigdecimal_to_u128!(r.guild_id),
-                created_at: r.created_at,
-                uses: r.uses,
-                max_uses: r.max_uses,
-                max_age: r.max_age,
-            },
-            code: 200,
-        })
-        .ok_or_else(|| {
-            (
-                404,
-                ErrorJson::new_404(
-                    format!("Unknown invite with code {}", code),
-                ),
-            )
-                .into()
-        })?)
+        .ok_or_else(|| ErrorJson::new_404(format!("Unknown invite with code {}", code)))?;
+    Ok(crate::Json {
+        obj: Invite {
+            code: code.to_string(),
+            owner_id: bigdecimal_to_u128!(r.owner_id),
+            guild_id: bigdecimal_to_u128!(r.guild_id),
+            created_at: r.created_at,
+            uses: r.uses,
+            max_uses: r.max_uses,
+            max_age: r.max_age,
+        },
+        code: 200,
+    })
 }
