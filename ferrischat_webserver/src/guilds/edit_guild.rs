2c33bc80630b294bae8947957768363c2fe1f5ab
0/0 <zero@imaskeleton.me>
2021-12-04 18:57:38 -0700

    fix edit_guild.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/guilds/edit_guild.rs
+++ ferrischat_webserver/src/guilds/edit_guild.rs
@@ -3,9 +3,8 @@ use crate::WebServerError;
 use axum::extract::Path;
 use axum::Json;
 use ferrischat_common::request_json::GuildUpdateJson;
-use ferrischat_common::types::{Guild, GuildFlags, ErrorJson};
+use ferrischat_common::types::{ErrorJson, Guild, GuildFlags};
 use ferrischat_common::ws::WsOutboundEvent;
-use serde::Serialize;
 
 pub async fn edit_guild(
     Path(guild_id): Path<u128>,
@@ -16,22 +15,19 @@ pub async fn edit_guild(
 
     let bigint_guild_id = u128_to_bigdecimal!(guild_id);
 
-    let old_guild_obj: Guild = sqlx::query!("SELECT * FROM guilds WHERE id = $1", bigint_guild_id)
+    let guild = sqlx::query!("SELECT * FROM guilds WHERE id = $1", bigint_guild_id)
         .fetch_optional(db)
         .await?
-        .map(|guild| Guild {
-            id: bigdecimal_to_u128!(guild.id),
-            owner_id: bigdecimal_to_u128!(guild.owner_id),
-            name: guild.name,
-            flags: GuildFlags::empty(),
-            channels: None,
-            members: None,
-            roles: None,
-        })
-        .ok_or_else(|| ErrorJson::new_404(
-            format!("Unknown guild with ID {}", guild_id),
-        ),
-        )?;
+        .ok_or_else(|| ErrorJson::new_404(format!("Unknown guild with ID {}", guild_id)))?;
+    let old_guild_obj = Guild {
+        id: bigdecimal_to_u128!(guild.id),
+        owner_id: bigdecimal_to_u128!(guild.owner_id),
+        name: guild.name,
+        flags: GuildFlags::empty(),
+        channels: None,
+        members: None,
+        roles: None,
+    };
 
     if let Some(name) = name {
         sqlx::query!(
@@ -39,26 +35,23 @@ pub async fn edit_guild(
             name,
             bigint_guild_id
         )
-            .execute(db)
-            .await?
-    };
+        .execute(db)
+        .await?;
+    }
 
-    let new_guild_obj: Guild = sqlx::query!("SELECT * FROM guilds WHERE id = $1", bigint_guild_id)
+    let guild = sqlx::query!("SELECT * FROM guilds WHERE id = $1", bigint_guild_id)
         .fetch_optional(db)
         .await?
-        .map(|guild| Guild {
-            id: bigdecimal_to_u128!(guild.id),
-            owner_id: bigdecimal_to_u128!(guild.owner_id),
-            name: guild.name,
-            flags: GuildFlags::empty(),
-            channels: None,
-            members: None,
-            roles: None,
-        })
-        .ok_or_else(|| ErrorJson::new_404(
-            format!("Unknown guild with ID {}", guild_id),
-        ),
-        )?;
+        .ok_or_else(|| ErrorJson::new_404(format!("Unknown guild with ID {}", guild_id)))?;
+    let new_guild_obj = Guild {
+        id: bigdecimal_to_u128!(guild.id),
+        owner_id: bigdecimal_to_u128!(guild.owner_id),
+        name: guild.name,
+        flags: GuildFlags::empty(),
+        channels: None,
+        members: None,
+        roles: None,
+    };
 
     // TODO: impl Eq for all types
     // if old_guild_obj == new_guild_obj {}
