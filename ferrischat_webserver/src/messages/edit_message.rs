e52d481a14a7d538dae9b36b4764388ee7448961
0/0 <zero@imaskeleton.me>
2021-12-04 19:22:59 -0700

    fix edit_message.rs
    Signed-off-by: 0/0 <zero@imaskeleton.me>

--- ferrischat_webserver/src/messages/edit_message.rs
+++ ferrischat_webserver/src/messages/edit_message.rs
@@ -5,13 +5,12 @@ use axum::Json;
 use ferrischat_common::request_json::MessageUpdateJson;
 use ferrischat_common::types::{ErrorJson, Message, User, UserFlags};
 use ferrischat_common::ws::WsOutboundEvent;
-use serde::Serialize;
 
 pub async fn edit_message(
     Path((channel_id, message_id)): Path<(u128, u128)>,
     Json(MessageUpdateJson { content }): Json<MessageUpdateJson>,
     auth: crate::Authorization,
-) -> Result<crate::Json<Message>, WebServerError<impl Serialize>> {
+) -> Result<crate::Json<Message>, WebServerError> {
     let bigint_channel_id = u128_to_bigdecimal!(channel_id);
     let bigint_message_id = u128_to_bigdecimal!(message_id);
 
@@ -21,17 +20,20 @@ pub async fn edit_message(
         if content.len() > 10240 {
             return Err(ErrorJson::new_400(
                 "message content size must be fewer than 10,240 bytes".to_string(),
-            ).into());
+            )
+            .into());
         }
     }
 
-    let guild_id = sqlx::query!(
-        "SELECT guild_id FROM channels WHERE id = $1",
-        bigint_channel_id
-    )
-    .fetch_one(db)
-    .await
-    .map(|x| bigdecimal_to_u128!(x))?;
+    let guild_id = bigdecimal_to_u128!(
+        sqlx::query!(
+            "SELECT guild_id FROM channels WHERE id = $1",
+            bigint_channel_id
+        )
+        .fetch_one(db)
+        .await?
+        .guild_id
+    );
 
     let resp = sqlx::query!(
         "SELECT m.*, a.name AS author_name, a.flags AS author_flags, a.discriminator AS author_discriminator, a.pronouns AS author_pronouns FROM messages m CROSS JOIN LATERAL (SELECT * FROM users WHERE id = m.author_id) AS a WHERE m.id = $1 AND m.channel_id = $2",
@@ -47,13 +49,10 @@ pub async fn edit_message(
     let old_message_obj = {
         let author_id = bigdecimal_to_u128!(resp.author_id);
         if author_id != auth.0 {
-            return (
-                403,
-                ErrorJson::new_403(
-                    "this user is not the author of the message".to_string(),
-                ),
+            return Err(ErrorJson::new_403(
+                "this user is not the author of the message".to_string(),
             )
-                .into();
+            .into());
         }
 
         let author_id = bigdecimal_to_u128!(resp.author_id);
@@ -80,23 +79,23 @@ pub async fn edit_message(
         }
     };
 
-    let new_msg_obj = sqlx::query!("UPDATE messages SET content = $1, edited_at = now()::timestamp without time zone WHERE channel_id = $2 AND id = $3 RETURNING *", content, bigint_channel_id, bigint_message_id)
+    let message = sqlx::query!("UPDATE messages SET content = $1, edited_at = now()::timestamp without time zone WHERE channel_id = $2 AND id = $3 RETURNING *", content, bigint_channel_id, bigint_message_id)
         .fetch_optional(db)
         .await?
-        .map(|message| Message {
-            id: message_id,
-            channel_id,
-            author_id: bigdecimal_to_u128!(message.author_id),
-            content: message.content,
-            edited_at: message.edited_at,
-            embeds: vec![],
-            author: old_message_obj.author.clone(),
-            nonce: None,
-        })
         .ok_or_else(|| ErrorJson::new_404(
             format!("Unknown message with ID {}", message_id)
         )
         )?;
+    let new_msg_obj = Message {
+        id: message_id,
+        channel_id,
+        author_id: bigdecimal_to_u128!(message.author_id),
+        content: message.content,
+        edited_at: message.edited_at,
+        embeds: vec![],
+        author: old_message_obj.author.clone(),
+        nonce: None,
+    };
 
     let event = WsOutboundEvent::MessageUpdate {
         old: old_message_obj,
