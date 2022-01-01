d28e5ab6c47cead170deef2517590ece33d72625
hydrostaticcog <hydro@hydrostaticcog.me>
2022-01-01 14:15:24 -0600

    restrict spaces in channel names
    Signed-off-by: hydrostaticcog <hydro@hydrostaticcog.me>

--- ferrischat_webserver/src/channels/create_channel.rs
+++ ferrischat_webserver/src/channels/create_channel.rs
@@ -3,7 +3,7 @@ use crate::{Json, WebServerError};
 use axum::extract::Path;
 use axum::Json as JsonInput;
 use ferrischat_common::request_json::ChannelCreateJson;
-use ferrischat_common::types::{Channel, ModelType};
+use ferrischat_common::types::{Channel, ErrorJson, ModelType};
 use ferrischat_common::ws::WsOutboundEvent;
 use ferrischat_macros::get_db_or_fail;
 use ferrischat_snowflake_generator::generate_snowflake;
@@ -17,6 +17,11 @@ pub async fn create_channel(
     let db = get_db_or_fail!();
 
     let ChannelCreateJson { name } = channel_info.0;
+    if name.contains(char::is_whitespace) {
+        return Err(
+            ErrorJson::new_400("A channel name may not contain a whitespace!".to_string()).into(),
+        );
+    }
 
     let node_id = get_node_id!();
     let channel_id = generate_snowflake::<0>(ModelType::Channel as u8, node_id);
