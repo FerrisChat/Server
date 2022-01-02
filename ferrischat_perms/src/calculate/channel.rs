77b49cc065e7666c3dd40d3bd852c534f39afe16
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-01-02 15:20:09 -0800

    Fix mismatched type

--- ferrischat_perms/src/calculate/channel.rs
+++ ferrischat_perms/src/calculate/channel.rs
@@ -6,7 +6,7 @@ impl<'a> PermissionCalculatorTM<'a> {
     pub fn to_channel(self) -> ChannelPermissions {
         let mut perms = ChannelPermissions::empty();
 
-        let mut roles = self.member.roles;
+        let mut roles = self.from_member.roles;
 
         roles.reverse();
 
@@ -21,6 +21,11 @@ impl<'a> PermissionCalculatorTM<'a> {
             let roles = roles.iter_mut().map(|x| x.id).collect::<Vec<u128>>();
 
             overwrites.reverse();
+
+            let user_id = match member.user_id {
+                Some(id) => id,
+                None => member.user.unwrap_or_else(|| unreachable!("No user_id and no user")).id,
+            };
 
             for (object, overwrite) in overwrites {
                 if object == member.user_id {
